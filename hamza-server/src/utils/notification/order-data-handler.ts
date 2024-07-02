570ce14cc5bf427d0bfb5142e1ec62d96e39bd25
karan chugh <karanchugh@karan-ka-MacBook-Pro.local>
2024-07-02 15:30:40 +0530

    ci: order confirmation mail fix

--- hamza-server/src/utils/notification/order-data-handler.ts
+++ hamza-server/src/utils/notification/order-data-handler.ts
@@ -1,24 +1,30 @@
 import moment from 'moment';
 import { Order } from 'src/models/order';
 import { formatCryptoPrice } from '../price-formatter';
+import fs from 'fs';
 
 export default function ordersDataParser(orders: Order[]) {
     let parsedData = {};
+
     orders.forEach((order) => {
         if (parsedData[order.store_id]) {
             parsedData[order.store_id].orders.push({
                 orderId: order.id,
-                items: order.cart.items.map((a) => {
-                    return {
-                        thumbnail: a.thumbnail,
-                        title: a.title,
-                        quantity: a.quantity,
-                        unit_price: `${formatCryptoPrice(
-                            a.unit_price,
-                            order.currency_code
-                        )} ${order.currency_code}`,
-                    };
-                }),
+                items: order.cart.items
+                    .filter(
+                        (a) => a.variant.product.store_id === order.store_id
+                    )
+                    .map((a) => {
+                        return {
+                            thumbnail: a.thumbnail,
+                            title: a.title,
+                            quantity: a.quantity,
+                            unit_price: `${formatCryptoPrice(
+                                a.unit_price,
+                                order.currency_code
+                            )} ${order.currency_code}`,
+                        };
+                    }),
                 orderDate: moment(order.created_at).format(
                     'MMMM Do YYYY, h:mm:ss a'
                 ),
@@ -27,34 +33,45 @@ export default function ordersDataParser(orders: Order[]) {
                     order.currency_code
                 )} ${order.currency_code}`,
             });
-
-            parsedData[order.store_id].totalStoreOrderAmount +=
-                order.payments[0].amount;
         } else {
             parsedData[order.store_id] = {
                 orders: [
                     {
                         orderId: order.id,
-                        items: order.cart.items.map((a) => {
-                            return {
-                                thumbnail: a.thumbnail,
-                                title: a.title,
-                                quantity: a.quantity,
-                                unit_price: a.unit_price,
-                            };
-                        }),
+                        items: order.cart.items
+                            .filter(
+                                (a) =>
+                                    a.variant.product.store_id ===
+                                    order.store_id
+                            )
+                            .map((a) => {
+                                return {
+                                    thumbnail: a.thumbnail,
+                                    title: a.title,
+                                    quantity: a.quantity,
+                                    unit_price: `${formatCryptoPrice(
+                                        a.unit_price,
+                                        order.currency_code
+                                    )} ${order.currency_code}`,
+                                };
+                            }),
                         orderDate: moment(order.created_at).format(
                             'MMMM Do YYYY, h:mm:ss a'
                         ),
-                        orderAmount: order.payments[0].amount,
+                        orderAmount: `${formatCryptoPrice(
+                            order.payments[0].amount,
+                            order.currency_code
+                        )} ${order.currency_code}`,
                     },
                 ],
                 storeName: order.store.name,
                 storeId: order.store_id,
-                totalStoreOrderAmount: order.payments[0].amount,
+                currencyCode: order.currency_code,
             };
         }
     });
 
+    // console.log('parsed data');
+    // console.dir(parsedData, { depth: null });
     return parsedData;
 }
