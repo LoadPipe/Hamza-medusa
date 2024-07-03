7164f9139b0a5abed6e720be3042a0f457d5fdf0
karan <karanchugh02@gmail.com>
2024-07-03 15:52:51 +0530

    ci: added wishlist icon

--- hamza-client/src/modules/products/components/product-preview/components/product-info.tsx
+++ hamza-client/src/modules/products/components/product-preview/components/product-info.tsx
@@ -10,9 +10,17 @@ import {
 } from '@chakra-ui/react';
 import React from 'react';
 import useProductPreview from '@store/product-preview/product-preview';
+import { BiHeart, BiSolidHeart } from 'react-icons/bi';
+import useWishlistStore from '@store/wishlist/wishlist-store';
+import { useWishlistMutations } from '@store/wishlist/mutations/wishlist-mutations';
+import { useCustomerAuthStore } from '@store/customer-auth/customer-auth';
 
 const ProductInfo = () => {
     const { productData } = useProductPreview();
+    const { wishlist } = useWishlistStore();
+    const { addWishlistItemMutation, removeWishlistItemMutation } =
+        useWishlistMutations();
+    const { authData } = useCustomerAuthStore();
 
     const isLoading = !productData || Object.keys(productData).length === 0;
 
@@ -54,13 +62,50 @@ const ProductInfo = () => {
                     <Text color="white">Electronics</Text>
                 </Box>
             </Flex>
-            <Heading
-                display={{ base: 'none', md: 'block' }}
-                as="h1"
-                color="white"
-            >
-                {productData.title}
-            </Heading>
+            <Flex className="flex justify-between items-center pr-5">
+                <Heading
+                    display={{ base: 'none', md: 'block' }}
+                    as="h1"
+                    color="white"
+                >
+                    {productData.title}
+                </Heading>
+
+                {authData.status == 'authenticated' && (
+                    <Box>
+                        {wishlist.products.find(
+                            (a) => a.id == productData.id
+                        ) ? (
+                            <BiSolidHeart
+                                onClick={() => {
+                                    removeWishlistItemMutation.mutate({
+                                        id: productData.id,
+                                        description: productData.description,
+                                        handle: productData.handle,
+                                        thumbnail: productData.thumbnail,
+                                        title: productData.title,
+                                    });
+                                }}
+                                className="text-white text-2xl cursor-pointer"
+                            />
+                        ) : (
+                            <BiHeart
+                                onClick={() => {
+                                    addWishlistItemMutation.mutate({
+                                        id: productData.id,
+                                        description: productData.description,
+                                        handle: productData.handle,
+                                        thumbnail: productData.thumbnail,
+                                        title: productData.title,
+                                    });
+                                }}
+                                className="text-white text-2xl cursor-pointer"
+                            />
+                        )}
+                    </Box>
+                )}
+            </Flex>
+
             <Flex flexDirection={'column'}>
                 <Heading
                     as="h2"
