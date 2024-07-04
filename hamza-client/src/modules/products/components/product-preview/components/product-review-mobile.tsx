0170731d986145d2abd7a9c1d0059df85f13aa7b
karan <karanchugh02@gmail.com>
2024-07-04 15:12:48 +0530

    replicated for mobile component

--- hamza-client/src/modules/products/components/product-preview/components/product-review-mobile.tsx
+++ hamza-client/src/modules/products/components/product-preview/components/product-review-mobile.tsx
@@ -1,29 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReviewCardMobile from './review-card-mobile';
 import { Text, Flex, Box } from '@chakra-ui/react';
-
-const reviews = [
-    {
-        id: 1,
-        name: 'John Doe',
-        location: 'New York',
-        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    },
-    {
-        id: 2,
-        name: 'Jane Smith',
-        location: 'California',
-        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    },
-    {
-        id: 3,
-        name: 'Alice Johnson',
-        location: 'Texas',
-        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    },
-];
+import axios from 'axios';
+import useProductPreview from '@store/product-preview/product-preview';
 
 const ProductReviewMobile = () => {
+    const { productId } = useProductPreview();
+
+    const [fakeReviews, setFakeReviews] = useState(true);
+    const [reviews, setReviews] = useState([
+        {
+            id: 1,
+            name: 'John Doe',
+            location: 'New York',
+            review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            stars: 4,
+        },
+        {
+            id: 2,
+            name: 'Jane Smith',
+            location: 'California',
+            stars: 4,
+            review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        },
+        {
+            id: 3,
+            name: 'Alice Johnson',
+            location: 'Texas',
+            stars: 4,
+            review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        },
+    ]);
+    const reviewDataFetcher = async () => {
+        let res = await axios.post(
+            `${process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL}/custom/review/all-reviews`,
+            {
+                product_id: productId,
+            }
+        );
+
+        if (res.data && res.data.length > 0) {
+            setReviews(
+                res.data.map((a: any) => {
+                    return {
+                        id: a.id,
+                        name:
+                            `${a.customer.first_name} ${a.customer.last_name}` ||
+                            'Anonymous Customer',
+                        location: 'US',
+                        review: a.content,
+                        stars: a.rating,
+                    };
+                })
+            );
+            setFakeReviews(false);
+        }
+
+        return;
+    };
+
+    useEffect(() => {
+        if (productId) {
+            reviewDataFetcher();
+        }
+    }, [productId]);
     return (
         <Flex
             maxW="1280px"
@@ -41,6 +81,7 @@ const ProductReviewMobile = () => {
                             name={review.name}
                             location={review.location}
                             review={review.review}
+                            stars={review.stars}
                         />
                     ))}
                 </Flex>
