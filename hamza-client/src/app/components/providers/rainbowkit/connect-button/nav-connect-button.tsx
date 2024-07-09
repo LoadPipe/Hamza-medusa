6053c9bdc3d618dcde20571e0b9e8577a30a8ffd
Jonatahn Bajada <jbajada@ryerson.ca>
2024-07-09 18:31:43 +0700

    updated new nav bar

---
+++ hamza-client/src/app/components/providers/rainbowkit/connect-button/nav-connect-button.tsx
@@ -0,0 +1,130 @@
+'use client';
+import { Box, Button, Text, Flex, Image } from '@chakra-ui/react';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { formatAddress } from '@lib/util/format-address';
+import { MdOutlineAccountBalanceWallet } from 'react-icons/md';
+
+export const WalletConnectButton = () => {
+    return (
+        <ConnectButton.Custom>
+            {({
+                account,
+                chain,
+                openAccountModal,
+                openChainModal,
+                openConnectModal,
+                authenticationStatus,
+                mounted,
+            }) => {
+                // Ensure the component is ready and not in a loading state
+                const ready = mounted && authenticationStatus !== 'loading';
+                const connected =
+                    ready &&
+                    account &&
+                    chain &&
+                    authenticationStatus === 'authenticated';
+
+                return (
+                    <Flex
+                        width={'203px'}
+                        height={'54px'}
+                        ml="1rem"
+                        {...(!ready && {
+                            'aria-hidden': true,
+                            style: {
+                                opacity: 0,
+                                pointerEvents: 'none',
+                                userSelect: 'none',
+                            },
+                        })}
+                    >
+                        {(() => {
+                            if (!connected) {
+                                return (
+                                    <Flex
+                                        width={'203px'}
+                                        height={'54px'}
+                                        borderRadius={'30px'}
+                                        justifyContent={'center'}
+                                        alignItems={'center'}
+                                        backgroundColor={'primary.green.900'}
+                                        onClick={openConnectModal}
+                                    >
+                                        <Text
+                                            alignSelf={'center'}
+                                            fontWeight={'600'}
+                                            fontSize={'20px'}
+                                        >
+                                            Connect Wallet
+                                        </Text>
+                                    </Flex>
+                                );
+                            }
+                            if (chain && chain.unsupported) {
+                                return (
+                                    <button
+                                        onClick={openChainModal}
+                                        type="button"
+                                    ></button>
+                                );
+                            }
+                            return (
+                                <Flex
+                                    px="1rem"
+                                    width={'100%'}
+                                    flexDirection={'row'}
+                                >
+                                    <Box
+                                        mr="0.5rem"
+                                        w="2.5rem"
+                                        h="2.5rem"
+                                        display={'flex'}
+                                        justifyContent={'center'}
+                                        borderRadius={'50%'}
+                                        alignSelf={'center'}
+                                        backgroundColor={'#2C272D'}
+                                    >
+                                        {chain.iconUrl && (
+                                            <Image
+                                                alignSelf={'center'}
+                                                alt={`${chain.name ?? 'Chain'} icon`}
+                                                src={chain.iconUrl}
+                                            />
+                                        )}
+                                    </Box>
+                                    <Flex
+                                        flex={1}
+                                        borderRadius={'0px'}
+                                        backgroundColor={'#2C272D'}
+                                        onClick={openAccountModal}
+                                        style={{
+                                            outline: 'none',
+                                            transition: 'all 0.3s ease',
+                                        }}
+                                    >
+                                        <Text
+                                            as="p"
+                                            alignSelf={'center'}
+                                            textStyle="h2"
+                                            color="#8C8C8C"
+                                        >
+                                            {formatAddress(account.address)}
+                                        </Text>
+                                        <Text
+                                            alignSelf={'center'}
+                                            ml="auto"
+                                            fontSize={'40px'}
+                                            color="primary.green.900"
+                                        >
+                                            &#x2022;
+                                        </Text>
+                                    </Flex>
+                                </Flex>
+                            );
+                        })()}
+                    </Flex>
+                );
+            }}
+        </ConnectButton.Custom>
+    );
+};
