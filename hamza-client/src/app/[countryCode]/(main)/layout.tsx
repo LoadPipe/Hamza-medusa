1fa72fe42a2066796f448baf7f0bda141555e274
Jonatahn Bajada <jbajada@ryerson.ca>
2024-07-16 16:18:04 +0700

    update layout

--- hamza-client/src/app/[countryCode]/(main)/layout.tsx
+++ hamza-client/src/app/[countryCode]/(main)/layout.tsx
@@ -1,11 +1,6 @@
 import { Metadata } from 'next';
 import Footer from '@modules/layout/templates/footer';
-<<<<<<< HEAD
-// import Nav from '@modules/layout/templates/nav-update';
-import Nav from '@modules/layout/templates/nav-4';
-=======
-import Nav from 'modules/layout/templates/nav';
->>>>>>> staging
+import Nav from 'modules/layout/templates/nav-4';
 import { Box } from '@chakra-ui/react';
 
 const MEDUSA_CLIENT_URL =
