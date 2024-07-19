290a27e776276c024cf7e2625d4ee184d4cca9f2
Garo <garonazarian09@gmail.com>
2024-07-19 14:14:48 +0700

    SSR VendorBanner

--- hamza-client/src/lib/data/index.ts
+++ hamza-client/src/lib/data/index.ts
@@ -379,6 +379,17 @@ export async function vendorReviews(store_id: string) {
     }
 }
 
+export async function getStoreName(product_id: string) {
+    try {
+        const response = await axios.post(`${BACKEND_URL}/custom/get-store`, {
+            product_id: product_id,
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching store name:', error);
+    }
+}
+
 // Cart actions
 export async function createCart(data = {}) {
     const headers = getMedusaHeaders(['cart']);
