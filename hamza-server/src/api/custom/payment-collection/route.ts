3693499efbaf62b58e2232ca8aba3c793d9016c8
Tronky <john.kosinski@gmail.com>
2024-07-19 18:13:29 +0700

    fixed bug: wrong path

--- hamza-server/src/api/custom/payment-collection/route.ts
+++ hamza-server/src/api/custom/payment-collection/route.ts
@@ -1,35 +1,28 @@
 import type { MedusaRequest, MedusaResponse, Logger } from '@medusajs/medusa';
 import { readRequestBody } from '../../../utils/request-body';
 import PaymentCollectionService from '../../../services/payment-collection';
-import { RouteHandler } from 'src/api/route-handler';
 
 export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
     const paymentCollectionService: PaymentCollectionService =
         req.scope.resolve('paymentCollectionService');
+    const logger: Logger = req.scope.resolve('logger');
 
-    const handler = new RouteHandler(
-        req,
-        res,
-        'POST',
-        '/custom/payment-collection',
-        ['region_id', 'type', 'currency_code', 'amount', 'created_by']
-    );
+    const payload = readRequestBody(req.body, [
+        'region_id',
+        'type',
+        'currency_code',
+        'amount',
+        'created_by',
+    ]);
 
-    await handler.handle(async () => {
-        const paymentCollection = await paymentCollectionService.create(
-            handler.inputParams.payload
-        );
+    try {
+        const paymentCollection =
+            await paymentCollectionService.create(payload);
         res.json(paymentCollection);
-    });
-
-    // try {
-    //     const paymentCollection =
-    //         await paymentCollectionService.create(payload);
-    //     res.json(paymentCollection);
-    // } catch (err) {
-    //     logger.error('Error creating payment collection', err);
-    //     res.status(500).json({
-    //         error: 'Failed to create payment collection',
-    //     });
-    // }
+    } catch (err) {
+        logger.error('Error creating payment collection', err);
+        res.status(500).json({
+            error: 'Failed to create payment collection',
+        });
+    }
 };
