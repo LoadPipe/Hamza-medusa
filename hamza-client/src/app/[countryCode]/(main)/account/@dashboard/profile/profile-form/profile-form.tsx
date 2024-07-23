eb891902873889876816e743f1b6168216d2fb75
Jonatahn Bajada <jbajada@ryerson.ca>
2024-07-23 18:34:23 +0700

    update profile-form clear states

--- hamza-client/src/app/[countryCode]/(main)/account/@dashboard/profile/profile-form/profile-form.tsx
+++ hamza-client/src/app/[countryCode]/(main)/account/@dashboard/profile/profile-form/profile-form.tsx
@@ -1,30 +1,53 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Flex, Button, Text } from '@chakra-ui/react';
 import ProfileInput from './components/profile-input';
 import ProfilePhoneInput from './components/phone-input';
 import useProfile from '@store/profile/profile';
 import { updateCustomer } from '@lib/data';
-import { customerKeys } from 'medusa-react';
 
 type Customer = {
-    id: string;
     first_name: string;
     last_name: string;
     email: string;
+    phone: string;
 };
 const ProfileForm = ({ customer }: { customer: Customer }) => {
-    const { firstName, lastName, email, phoneNumber } = useProfile();
+    const {
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        setFirstName,
+        setLastName,
+        setEmail,
+        setPhoneNumber,
+    } = useProfile();
+
+    const clearStates = async () => {
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setPhoneNumber('');
+    };
+
     const handleSubmit = async () => {
+        if (!firstName && !lastName && !email && !phoneNumber) {
+            alert('No changes');
+            return;
+        }
+
         try {
             const updatedCustomer = {
-                first_name: firstName,
-                last_name: 'boy',
-                email: 'johnbajada01@gmail.com',
+                first_name: firstName || customer.first_name,
+                last_name: lastName || customer.last_name,
+                email: email || customer.email,
+                phone: phoneNumber || customer.phone,
             };
             await updateCustomer(updatedCustomer);
-            alert('Profile updated successfully');
             console.log(customer);
+            alert('Profile updated successfully');
+            clearStates();
         } catch (error) {
             alert('Failed to update profile');
             console.error('Error updating profile:', error);
