343f1f547bc6cb7b0538b19abdf09829be05a4e9
Jonatahn Bajada <jbajada@ryerson.ca>
2024-07-23 14:18:59 +0700

    updated error messages on profile

--- hamza-client/src/app/[countryCode]/(main)/account/@dashboard/profile/components/profile-input.tsx
+++ hamza-client/src/app/[countryCode]/(main)/account/@dashboard/profile/components/profile-input.tsx
@@ -1,7 +1,14 @@
 'use client';
 
-import { Flex, Text, Input } from '@chakra-ui/react';
-import React, { useState, ChangeEvent, useEffect } from 'react';
+import {
+    Flex,
+    Text,
+    Input,
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+} from '@chakra-ui/react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import useProfile from '@store/profile/profile';
 
 interface ProfileInputProps {
@@ -11,22 +18,32 @@ interface ProfileInputProps {
 const ProfileInput: React.FC<ProfileInputProps> = ({ label }) => {
     const { firstName, lastName, email, setFirstName, setLastName, setEmail } =
         useProfile();
+    const [error, setError] = useState<string | null>(null);
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         const value = event.target.value;
+        setError(null); // Reset error on input change
 
         switch (label.toLowerCase()) {
             case 'first name':
                 setFirstName(value);
+                if (value.trim() === '') setError('First Name is required');
                 break;
             case 'last name':
                 setLastName(value);
+                if (value.trim() === '') setError('Last Name is required');
                 break;
             case 'email':
                 setEmail(value);
                 break;
             default:
                 break;
+        }
+    };
+
+    const handleBlur = () => {
+        if (label.toLowerCase() === 'email' && !/\S+@\S+\.\S+/.test(email)) {
+            setError('Email is invalid');
         }
     };
 
@@ -38,29 +55,33 @@ const ProfileInput: React.FC<ProfileInputProps> = ({ label }) => {
 
     return (
         <Flex flexDirection={'column'} maxW={'420px'} width={'100%'}>
-            <Text
-                textTransform={'uppercase'}
-                fontSize={'12px'}
-                pl="1rem"
-                mb={'8px'}
-            >
-                {label}
-            </Text>
-            <Input
-                borderRadius={'12px'}
-                height={'52px'}
-                backgroundColor={'#020202'}
-                border={'none'}
-                type={label.toLowerCase() === 'email' ? 'email' : 'text'}
-                value={
-                    label.toLowerCase() === 'first name'
-                        ? firstName
-                        : label.toLowerCase() === 'last name'
-                          ? lastName
-                          : email
-                }
-                onChange={handleChange}
-            />
+            <FormControl isInvalid={!!error}>
+                <FormLabel
+                    textTransform={'uppercase'}
+                    fontSize={'12px'}
+                    pl="1rem"
+                    mb={'8px'}
+                >
+                    {label}
+                </FormLabel>
+                <Input
+                    borderRadius={'12px'}
+                    height={'52px'}
+                    backgroundColor={'#020202'}
+                    border={'none'}
+                    type={label.toLowerCase() === 'email' ? 'email' : 'text'}
+                    value={
+                        label.toLowerCase() === 'first name'
+                            ? firstName
+                            : label.toLowerCase() === 'last name'
+                              ? lastName
+                              : email
+                    }
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                />
+                {error && <FormErrorMessage>{error}</FormErrorMessage>}
+            </FormControl>
         </Flex>
     );
 };
