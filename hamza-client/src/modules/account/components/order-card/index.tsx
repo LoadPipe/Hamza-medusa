299768ec2fb6ec39ab00f1379d17f163ae88f349
Garo <garonazarian09@gmail.com>
2024-07-23 18:14:01 +0700

    Refactor: Order Card Styling

--- hamza-client/src/modules/account/components/order-card/index.tsx
+++ hamza-client/src/modules/account/components/order-card/index.tsx
@@ -1,9 +1,5 @@
-import Thumbnail from '@modules/products/components/thumbnail';
 import { formatAmount } from '@lib/util/prices';
 import { formatCryptoPrice } from '@lib/util/get-product-price';
-import LocalizedClientLink from '@modules/common/components/localized-client-link';
-import { ExternalLinkIcon } from '@chakra-ui/icons';
-
 import {
     Box,
     Flex,
@@ -13,6 +9,9 @@ import {
     Link,
     useColorModeValue,
 } from '@chakra-ui/react';
+import { FaCheckCircle } from 'react-icons/fa';
+import React, { useEffect, useState } from 'react';
+import { getStoreName } from '@lib/data';
 // Update the type definitions to reflect the structure of the received order
 type OrderDetails = {
     thumbnail: string;
@@ -47,65 +46,99 @@ const OrderCard = ({ order, handle }: OrderCardProps) => {
     if (!order) {
         return <div>Loading...</div>; // Display loading message if order is undefined
     }
+    const [vendor, setVendor] = useState('');
     const textColor = useColorModeValue('gray.800', 'white');
-    const borderColor = useColorModeValue('gray.200', 'gray.700');
-    // console.log(`Order Card information is: ${JSON.stringify(order)}`);
     const orderString = typeof order.currency_code;
-    // console.log(
-    //     `Order Unit Price ${order.unit_price} and Currency Code ${order.currency_code} ${orderString}`
-    // );
-    console.log(`Order Card details ${JSON.stringify(order)}`);
+    console.log(
+        `Order Card details ${JSON.stringify(order.variant.product_id)}`
+    );
     console.log(`Product details ${JSON.stringify(handle)} `);
 
+    useEffect(() => {
+        // Fetch Vendor Name from product.id
+        const fetchVendor = async () => {
+            try {
+                const data = await getStoreName(
+                    order.variant.product_id as string
+                );
+                // console.log(`Vendor: ${data}`);
+                setVendor(data);
+            } catch (error) {
+                console.error('Error fetching vendor: ', error);
+            }
+        };
+
+        fetchVendor();
+    }, [order]);
     return (
         <Box
-            bg={useColorModeValue('white', 'gray.800')}
-            p={5}
+            bg={'black'}
+            color={'white'}
+            p={4}
             rounded="lg"
             shadow="base"
+            maxWidth="1000px"
+            m="auto"
         >
             <Flex alignItems="center" justifyContent="space-between">
-                <Box flexShrink={0}>
-                    <Image
-                        borderRadius="lg"
-                        width={{ base: 24, md: 48 }}
-                        src={order.thumbnail}
-                        alt={`Thumbnail of ${order.title}`}
-                    />
-                </Box>
-                <Box flex="1" ml={5}>
-                    <Text fontWeight="bold" fontSize="lg" color={textColor}>
-                        {order.title}
-                    </Text>
-                    <Text fontSize="sm" color={textColor}>
-                        {order.description}
-                    </Text>
-                    <Flex align="center" mt={2}>
-                        <Text
-                            fontSize="sm"
-                            color={textColor}
-                            pr={2}
-                            borderRight="1px"
-                            borderColor={borderColor}
-                        >
-                            {new Date(order.created_at).toLocaleDateString()}
-                        </Text>
-                        <Text fontSize="sm" color={textColor} px={2}>
+                <Image
+                    borderRadius="lg"
+                    width={{ base: '60px', md: '120px' }}
+                    src={order.thumbnail}
+                    alt={`Thumbnail of ${order.title}`}
+                    mr={4}
+                />
+
+                <Box flex="1">
+                    <Flex justifyContent="space-between" alignItems="center">
+                        <Flex alignItems="center">
+                            <Text
+                                fontSize={{ base: '14px', md: '24px' }}
+                                fontWeight="bold"
+                                noOfLines={1}
+                            >
+                                {vendor}
+                            </Text>
+                            <Flex
+                                display={{ base: 'none', md: 'flex' }}
+                                ml={2}
+                                alignItems="center"
+                            >
+                                <FaCheckCircle color="#3196DF" />
+                            </Flex>
+                        </Flex>
+                        <Text fontSize="md" fontWeight="semibold">
                             {order.unit_price} {order.currency_code}
                         </Text>
-                        <Text fontSize="sm" color={textColor} pl={2}>
-                            {order.quantity} items
+                    </Flex>
+
+                    <Flex direction="column" mt={2}>
+                        <Text fontWeight="bold" fontSize="lg">
+                            {order.title}
                         </Text>
+                        <Text fontSize="sm">{order.description}</Text>
+                    </Flex>
+
+                    <Flex
+                        justifyContent="space-between"
+                        alignItems="center"
+                        mt={2}
+                    >
+                        <Text fontSize="sm">
+                            {new Date(order.created_at).toLocaleDateString()}
+                        </Text>
+                        <Text fontSize="sm">{order.quantity} item(s)</Text>
                     </Flex>
                 </Box>
-                <Flex alignItems="center">
-                    <Button colorScheme="blue" mr={2}>
-                        Buy Again
-                    </Button>
-                    <Link href={`/contact-seller/${order.id}`} isExternal>
-                        Contact Seller <ExternalLinkIcon mx="2px" />
-                    </Link>
-                </Flex>
+            </Flex>
+
+            <Flex justifyContent="flex-end" mt={2}>
+                <Button colorScheme="blue" mr={2}>
+                    Buy Again
+                </Button>
+                <Button colorScheme="blue" mr={2}>
+                    Contact Seller
+                </Button>
             </Flex>
         </Box>
     );
