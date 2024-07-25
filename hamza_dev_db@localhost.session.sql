4a2dbd2cb90a12fdf0bdf1a2a38b53fdbf11e9c1
Jonatahn Bajada <jbajada@ryerson.ca>
2024-07-25 18:58:45 +0700

    pushed local db by accident

--- hamza_dev_db@localhost.session.sql
+++
@@ -1,9 +0,0 @@
--- UPDATE customer
--- SET email = 'johnbajada01@gmail.com'
--- WHERE id = 'cus_01J3MPZ85ZNCFEXEE6AFVX5CMV';
-
-UPDATE customer
-SET is_verified = FALSE
-WHERE id = 'cus_01J3MPZ85ZNCFEXEE6AFVX5CMV';
-
-SELECT * FROM customer;
