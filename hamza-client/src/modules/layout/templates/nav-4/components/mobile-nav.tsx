90e9019a21a1686aed40bbed982dee504b39e24c
Jonatahn Bajada <jbajada@ryerson.ca>
2024-08-06 16:20:16 +0700

    redisgned mobile wallet button

--- hamza-client/src/modules/layout/templates/nav-4/components/mobile-nav.tsx
+++ hamza-client/src/modules/layout/templates/nav-4/components/mobile-nav.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import HamzaLogo from '../../../../../../public/images/logo/logo_green.svg';
 import HamzaTitle from '../../../../../../public/images/logo/hamza-title.svg';
 import MobileMenu from '../components/mobile-menu';
-import ConnectWallet from '../connect-button';
+import ConnectWallet from '../connect-mobile';
 // import CartButton from '@modules/layout/components/cart-button';
 import CartButton from '@modules/layout/components/cart-button-mobile';
 export default async function MobileNav() {
