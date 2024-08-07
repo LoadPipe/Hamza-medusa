31c11bd619d3da331ca72f8394b72d325870f5c3
Garo <garonazarian09@gmail.com>
2024-08-07 12:12:34 -0400

    caching eth->usdc && eth->usdt

--- currency-conversion/src/coin-gecko.controller.ts
+++ currency-conversion/src/coin-gecko.controller.ts
@@ -1,61 +1,63 @@
 import {
-    Controller,
-    Get,
-    Query,
-    HttpException,
-    HttpStatus,
+  Controller,
+  Get,
+  Query,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { CoinGeckoService } from './coin-gecko.service';
 
 @Controller('convert')
 export class CoinGeckoController {
-    constructor(private readonly coinGeckoService: CoinGeckoService) { }
+  constructor(private readonly coinGeckoService: CoinGeckoService) {}
 
-    @Get('/convert')
-    async convertCurrencies(
-        @Query('base') baseCurrency: string,
-        @Query('to') conversionCurrency: string,
-    ): Promise<string> {
-        if (!baseCurrency || !conversionCurrency) {
-            throw new HttpException(
-                'Base currency and conversion currency must be provided',
-                HttpStatus.BAD_REQUEST,
-            );
-        }
-        try {
-            return await this.coinGeckoService.convertCurrency(
-                baseCurrency,
-                conversionCurrency,
-            );
-        } catch (error) {
-            throw new HttpException(
-                'Failed to convert currencies',
-                HttpStatus.INTERNAL_SERVER_ERROR,
-            );
-        }
+  @Get('/convert')
+  async convertCurrencies(
+    @Query('base') baseCurrency: string,
+    @Query('to') conversionCurrency: string,
+  ): Promise<string> {
+    if (!baseCurrency || !conversionCurrency) {
+      throw new HttpException(
+        'Base currency and conversion currency must be provided',
+        HttpStatus.BAD_REQUEST,
+      );
     }
+    try {
+      const rate = await this.coinGeckoService.getExchangeRate(
+        baseCurrency,
+        conversionCurrency,
+      );
+      const convertAmount = rate * 1; // Assuming 1 as the base amount
+      return `${convertAmount} ${conversionCurrency}`;
+    } catch (error) {
+      throw new HttpException(
+        'Failed to convert currencies',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
 
-    @Get('/exch')
-    async getExchangeRate(
-        @Query('base') baseCurrency: string,
-        @Query('to') conversionCurrency: string,
-    ): Promise<number> {
-        if (!baseCurrency || !conversionCurrency) {
-            throw new HttpException(
-                'Base currency and conversion currency must be provided',
-                HttpStatus.BAD_REQUEST,
-            );
-        }
-        try {
-            return await this.coinGeckoService.getExchangeRate(
-                baseCurrency,
-                conversionCurrency,
-            );
-        } catch (error) {
-            throw new HttpException(
-                'Failed to get exchange rate',
-                HttpStatus.INTERNAL_SERVER_ERROR,
-            );
-        }
+  @Get('/exch')
+  async getExchangeRate(
+    @Query('base') baseCurrency: string,
+    @Query('to') conversionCurrency: string,
+  ): Promise<number> {
+    if (!baseCurrency || !conversionCurrency) {
+      throw new HttpException(
+        'Base currency and conversion currency must be provided',
+        HttpStatus.BAD_REQUEST,
+      );
     }
+    try {
+      return await this.coinGeckoService.getExchangeRate(
+        baseCurrency,
+        conversionCurrency,
+      );
+    } catch (error) {
+      throw new HttpException(
+        'Failed to get exchange rate',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
 }
--- currency-conversion/src/coin-gecko.service.ts
+++ currency-conversion/src/coin-gecko.service.ts
@@ -3,114 +3,156 @@ import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 
 interface CacheEntry {
-    value: number; // Assuming conversion rates are numbers
-    timestamp: number;
+  value: number; // Assuming conversion rates are numbers
+  timestamp: number;
 }
 
 @Injectable()
-//TODO: rename 
+//TODO: rename
 export class CoinGeckoService {
-    private readonly logger = new Logger(CoinGeckoService.name);
-    private cache: { [key: string]: CacheEntry } = {};
-    private readonly cacheDuration = 300000; // Cache duration in milliseconds (5 minutes)
+  private readonly logger = new Logger(CoinGeckoService.name);
+  private cache: { [key: string]: CacheEntry } = {};
+  private readonly cacheDuration = 300000; // Cache duration in milliseconds (5 minutes)
 
-    // Correct contract addresses
-    private readonly USDT = '0xdac17f958d2ee523a2206206994597c13d831ec7';
-    private readonly USDC = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
-    private readonly ETH = '0x0000000000000000000000000000000000000000'; // Placeholder for native ETH
+  // Correct contract addresses
+  private readonly USDT = '0xdac17f958d2ee523a2206206994597c13d831ec7';
+  private readonly USDC = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
+  private readonly ETH = '0x0000000000000000000000000000000000000000'; // Placeholder for native ETH
 
-    constructor(private readonly httpService: HttpService) {
-        //cache here on startup 
-        //this.fetchConversionRate('eth', USDT'); //2517.34
-        //this.fetchConversionRate('eth', USDC'); //2517.331
+  constructor(private readonly httpService: HttpService) {
+    this.initCache();
+  }
+
+  private async initCache(): Promise<void> {
+    this.logger.log('Initializing cache...');
+    await Promise.all([
+      this.fetchAndCacheConversionRate(this.ETH, this.USDT),
+      this.fetchAndCacheConversionRate(this.ETH, this.USDC),
+    ])
+      .then(() => {
+        this.logger.log('Cache initialized for ETH to USDT and ETH to USDC.');
+      })
+      .catch((error) => {
+        this.logger.error('Failed to populate cache at startup:', error);
+      });
+  }
+
+  private async fetchAndCacheConversionRate(
+    contractAddress: string,
+    vsCurrency: string,
+  ): Promise<void> {
+    try {
+      const rate = await this.fetchConversionRate(contractAddress, vsCurrency);
+      const cacheKey = `${contractAddress}-${vsCurrency}`;
+      this.cache[cacheKey] = { value: rate, timestamp: new Date().getTime() };
+      this.logger.log(`Cached ${contractAddress} to ${vsCurrency} rate.`);
+    } catch (error) {
+      this.logger.error(
+        `Failed to fetch and cache rate for ${contractAddress} to ${vsCurrency}:`,
+        error,
+      );
+    }
+  }
+
+  async convertCurrency(
+    baseCurrency: string,
+    conversionCurrency: string,
+  ): Promise<string> {
+    if (baseCurrency === this.ETH || conversionCurrency === this.ETH) {
+      // Handle conversion involving native ETH
+      return this.handleEthConversion(baseCurrency, conversionCurrency);
+    } else {
+      // Convert between tokens
+      return this.convertTokenToToken(baseCurrency, conversionCurrency);
+    }
+  }
+
+  //this method will mainly be used
+  async getExchangeRate(
+    baseCurrency: string,
+    conversionCurrency: string,
+  ): Promise<number> {
+    // check for cached first
+    // use the inverse if we have a reversed version of the pair already cached
+    // inverse of exchange rate: 1/rate (e.g. eth->usdc is 3000, usdc->eth is 1/3000)
+    return await this.fetchConversionRate(baseCurrency, conversionCurrency);
+  }
+
+  private async handleEthConversion(
+    baseCurrency: string,
+    conversionCurrency: string,
+  ): Promise<string> {
+    // Determine if ETH is the base or the target and call the appropriate conversion function
+    if (baseCurrency === this.ETH) {
+      // ETH to Token
+      return this.fetchConversionRate(conversionCurrency, 'eth').then((rate) =>
+        (1 / rate).toString(),
+      );
+    } else {
+      // Token to ETH
+      return this.fetchConversionRate(baseCurrency, 'eth').then((rate) =>
+        rate.toString(),
+      );
+    }
+  }
+
+  private async convertTokenToToken(
+    baseCurrency: string,
+    conversionCurrency: string,
+  ): Promise<string> {
+    const baseToEth = await this.fetchConversionRate(baseCurrency, 'eth');
+    const conversionToEth = await this.fetchConversionRate(
+      conversionCurrency,
+      'eth',
+    );
+    const rate = baseToEth / conversionToEth;
+    return rate.toString();
+  }
+
+  private async fetchConversionRate(
+    contractAddress: string,
+    vsCurrency: string,
+  ): Promise<number> {
+    const cacheKey = `${contractAddress}-${vsCurrency}`;
+    const cachedData = this.cache[cacheKey];
+    const currentTime = new Date().getTime();
+
+    if (cachedData && currentTime - cachedData.timestamp < this.cacheDuration) {
+      console.log(`Using cached data for ${contractAddress} to ${vsCurrency}`);
+      return cachedData.value;
     }
 
-    async convertCurrency(
-        baseCurrency: string,
-        conversionCurrency: string,
-    ): Promise<string> {
-        if (baseCurrency === this.ETH || conversionCurrency === this.ETH) {
-            // Handle conversion involving native ETH
-            return this.handleEthConversion(baseCurrency, conversionCurrency);
-        } else {
-            // Convert between tokens
-            return this.convertTokenToToken(baseCurrency, conversionCurrency);
-        }
+    let url;
+    // Adjusting the endpoint based on whether the contract address is for Ethereum
+    if (contractAddress === this.ETH) {
+      url = `https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=${vsCurrency}`;
+    } else {
+      url = `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${contractAddress}&vs_currencies=${vsCurrency}`;
     }
 
-    //this method will mainly be used 
-    async getExchangeRate(
-        baseCurrency: string,
-        conversionCurrency: string,
-    ): Promise<number> {
-        // check for cached first 
-        // use the inverse if we have a reversed version of the pair already cached
-        // inverse of exchange rate: 1/rate (e.g. eth->usdc is 3000, usdc->eth is 1/3000)
-        return await this.fetchConversionRate(baseCurrency, conversionCurrency);
-    }
-
-    private async handleEthConversion(
-        baseCurrency: string,
-        conversionCurrency: string,
-    ): Promise<string> {
-        // Determine if ETH is the base or the target and call the appropriate conversion function
-        if (baseCurrency === this.ETH) {
-            // ETH to Token
-            return this.fetchConversionRate(conversionCurrency, 'eth').then((rate) =>
-                (1 / rate).toString(),
-            );
-        } else {
-            // Token to ETH
-            return this.fetchConversionRate(baseCurrency, 'eth').then((rate) =>
-                rate.toString(),
-            );
-        }
-    }
-
-    private async convertTokenToToken(
-        baseCurrency: string,
-        conversionCurrency: string,
-    ): Promise<string> {
-        const baseToEth = await this.fetchConversionRate(baseCurrency, 'eth');
-        const conversionToEth = await this.fetchConversionRate(
-            conversionCurrency,
-            'eth',
+    try {
+      const response = await firstValueFrom(this.httpService.get(url));
+      if (
+        !response.data ||
+        (contractAddress !== this.ETH && !response.data[contractAddress]) ||
+        (contractAddress === this.ETH && !response.data.ethereum)
+      ) {
+        throw new Error(
+          `No data found for ${contractAddress} with currency ${vsCurrency}`,
         );
-        const rate = baseToEth / conversionToEth;
-        return rate.toString();
+      }
+
+      const newValue =
+        contractAddress === this.ETH
+          ? response.data.ethereum[vsCurrency]
+          : response.data[contractAddress][vsCurrency];
+      this.cache[cacheKey] = { value: newValue, timestamp: currentTime };
+      return newValue;
+    } catch (error) {
+      this.logger.error(`Error fetching data for ${contractAddress}`, error);
+      throw new Error(
+        `An error occurred while fetching data for ${contractAddress}`,
+      );
     }
-
-    private async fetchConversionRate(
-        contractAddress: string,
-        vsCurrency: string,
-    ): Promise<number> {
-        const cacheKey = `${contractAddress}-${vsCurrency}`;
-        const cachedData = this.cache[cacheKey];
-        const currentTime = new Date().getTime();
-
-        // Check if data is in cache and not expired
-        if (cachedData && currentTime - cachedData.timestamp < this.cacheDuration) {
-            return cachedData.value;
-        }
-
-        // If not in cache or cache is expired, fetch new data
-        const url = `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${contractAddress}&vs_currencies=${vsCurrency}`;
-        try {
-            const response = await firstValueFrom(this.httpService.get(url));
-            const newValue = response.data[contractAddress][vsCurrency];
-
-            // Update cache with new value
-            this.cache[cacheKey] = {
-                value: newValue,
-                timestamp: currentTime,
-            };
-
-            return newValue;
-        } catch (error) {
-            this.logger.error(`Error fetching data for ${contractAddress}`, error);
-            throw new Error(
-                `An error occurred while fetching data for ${contractAddress}`,
-            );
-        }
-    }
+  }
 }
