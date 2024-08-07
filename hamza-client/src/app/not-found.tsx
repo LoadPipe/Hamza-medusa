d9fb897dcbe0a6152cda8ceb08e4bbdfa694a055
Jonatahn Bajada <jbajada@ryerson.ca>
2024-08-07 13:08:23 +0700

    updated 404 design

--- hamza-client/src/app/not-found.tsx
+++ hamza-client/src/app/not-found.tsx
@@ -1,20 +1,51 @@
-import { Metadata } from "next"
+import { Metadata } from 'next';
+import { Flex, Box, Text } from '@chakra-ui/react';
 
-import InteractiveLink from "@modules/common/components/interactive-link"
+import InteractiveLink from '@modules/common/components/interactive-link';
 
 export const metadata: Metadata = {
-  title: "404",
-  description: "Something went wrong",
-}
+    title: '404',
+    description: 'Something went wrong',
+};
 
 export default function NotFound() {
-  return (
-    <div className="flex flex-col gap-4 items-center justify-center min-h-[calc(100vh-64px)]">
-      <h1 className="text-2xl-semi text-ui-fg-base">Page not found</h1>
-      <p className="text-small-regular text-ui-fg-base">
-        The page you tried to access does not exist.
-      </p>
-      <InteractiveLink href="/">Go to homepage</InteractiveLink>
-    </div>
-  )
+    return (
+        <Flex
+            justifyContent={'center'}
+            alignItems={'center'}
+            backgroundColor={'black'}
+            color={'white'}
+            height={'100vh'}
+        >
+            <Flex
+                flexDir={'column'}
+                justifyContent={'center'}
+                alignItems={'center'}
+                maxWidth={'504px'}
+                width={'100%'}
+                gap={'10px'}
+            >
+                <Text color={'primary.green.900'} fontSize={'40px'}>
+                    Page not found
+                </Text>
+                <Text textAlign={'center'}>
+                    We're sorry, the page you requested could not be found.
+                    Please go back to the home page
+                </Text>
+                <Flex
+                    mt="1rem"
+                    width={'190px'}
+                    height={'50px'}
+                    backgroundColor={'primary.indigo.900'}
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                    borderRadius={'full'}
+                >
+                    <a href="/">
+                        <Text>Go Home</Text>
+                    </a>
+                </Flex>
+            </Flex>
+        </Flex>
+    );
 }
