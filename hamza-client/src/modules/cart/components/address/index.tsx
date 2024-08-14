82928300e1f8fcb4b8863a892725cd102f69e6d1
Jonatahn Bajada <jbajada@ryerson.ca>
2024-08-14 18:09:44 +0700

    updated address component

--- hamza-client/src/modules/cart/components/address/index.tsx
+++ hamza-client/src/modules/cart/components/address/index.tsx
@@ -22,15 +22,27 @@ const CartShippingAddress = ({
                 Shipping Address
             </Text>
             {customer ? (
-                <Text color="white">
+                <Text color="white" mt="1rem">
                     <Text
                         color={'primary.indigo.900'}
                         fontSize={'24px'}
                         fontWeight={900}
                     >
-                        {customer.shipping_addresses[0].first_name}{' '}
+                        {customer.shipping_addresses[0].first_name}
                         {customer.shipping_addresses[0].last_name}
                     </Text>
+                    {customer.shipping_addresses[0].email && (
+                        <>
+                            {customer.shipping_addresses[0].email}
+                            <br />
+                        </>
+                    )}
+                    {customer.shipping_addresses[0].phone && (
+                        <>
+                            {customer.shipping_addresses[0].phone}
+                            <br />
+                        </>
+                    )}
                     {customer.shipping_addresses[0].address_1}
                     <br />
                     {customer.shipping_addresses[0].address_2 && (
