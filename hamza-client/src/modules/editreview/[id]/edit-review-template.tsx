facc4afe98fe5ce7c90f9ba5c1ad6969e08a4b90
Garo <garonazarian09@gmail.com>
2024-08-20 12:50:03 -0400

    Edit Review fix

--- hamza-client/src/modules/editreview/[id]/edit-review-template.tsx
+++ hamza-client/src/modules/editreview/[id]/edit-review-template.tsx
@@ -25,36 +25,38 @@ const EditReviewTemplate = ({ review, isOpen, onClose }: any) => {
     const [currentReview, setCurrentReview] = useState(review.content || '');
     const [rating, setRating] = useState(review.rating || 0);
     const [hovered, setHovered] = useState(0);
-    const [submissionSuccess, setSubmissionSuccess] = useState(false);
 
     useEffect(() => {
-        // No need to fetch details if they are already passed as props
-        console.log(`Review issss?D: ${JSON.stringify(review)}`);
-        setRating(review.rating);
+        if (
+            review &&
+            (review.content !== currentReview || review.rating !== rating)
+        ) {
+            setCurrentReview(review.content || '');
+            setRating(review.rating || 0);
+        }
     }, [review]);
 
     console.log(`Review edit page ${JSON.stringify(review)}`);
 
     const submitReview = async () => {
+        console.log(
+            `Review data is ${review.product_id} ${currentReview} ${Number(rating)} ${review.customer_id} ${review.order_id}`
+        );
         try {
             const response = await updateProductReview(
                 review.product_id,
                 currentReview,
-                rating,
+                Number(rating),
                 review.customer_id,
                 review.order_id
             );
 
-            if (response.status === 200) {
-                setCurrentReview('');
-                setRating(0);
-                toast.success('Review Submitted!', {});
-                onClose();
-                console.log('Review updated successfully');
-            } else {
-                console.error('Failed to update review');
-                toast.error('Failed to submit Review.');
-            }
+            console.log(`RESPONSE IS ${response}`);
+
+            setCurrentReview('');
+            setRating(0);
+            toast.success('Review Updated Successfully!', {});
+            onClose();
         } catch (error) {
             console.error('Failed to submit review: ', error);
             toast.error('Failed to submit Review.');
@@ -74,105 +76,74 @@ const EditReviewTemplate = ({ review, isOpen, onClose }: any) => {
         <Modal isOpen={isOpen} onClose={onClose} isCentered>
             <ModalOverlay />
             <ModalContent>
-                {!submissionSuccess ? (
-                    <>
-                        <ModalHeader>Edit Your Review</ModalHeader>
-                        <ModalCloseButton />
-                        <ModalBody>
-                            <>
-                                <Box className="flex items-center mb-4">
-                                    <Image
-                                        src={review.product.thumbnail}
-                                        alt={review.title}
-                                        boxSize="96px"
-                                        mr="4"
-                                    />
-                                    <Box>
-                                        <Text
-                                            fontSize="xl"
-                                            fontWeight="semibold"
-                                        >
-                                            {review.title}
-                                        </Text>
-                                        <Text
-                                            dangerouslySetInnerHTML={{
-                                                __html:
-                                                    review.description || '',
-                                            }}
-                                        ></Text>
-                                    </Box>
-                                </Box>
-                                <Box>
-                                    <Box className="flex items-center mb-2">
-                                        {[1, 2, 3, 4, 5].map((star: number) => (
-                                            <Button
-                                                key={star}
-                                                size="lg"
-                                                variant={
-                                                    star <= (hovered || rating)
-                                                        ? 'solid'
-                                                        : 'ghost'
-                                                }
-                                                colorScheme={
-                                                    star <= (hovered || rating)
-                                                        ? 'yellow'
-                                                        : 'gray'
-                                                }
-                                                onClick={() => setRating(star)}
-                                                onMouseEnter={() =>
-                                                    setHovered(star)
-                                                }
-                                                onMouseLeave={() =>
-                                                    setHovered(0)
-                                                }
-                                            >
-                                                ★
-                                            </Button>
-                                        ))}
-                                        <Text
-                                            ml="2"
-                                            fontSize="sm"
-                                            fontWeight="medium"
-                                        >
-                                            {ratingDescriptions[rating - 1] ||
-                                                ''}
-                                        </Text>
-                                    </Box>
-                                    <Text fontSize="md" fontWeight="bold">
-                                        Review Detail
-                                    </Text>
-                                    <textarea
-                                        className="w-full p-2 border rounded text-black"
-                                        rows={4}
-                                        placeholder="What do you think of this product?"
-                                        value={currentReview}
-                                        onChange={(e) =>
-                                            setCurrentReview(e.target.value)
-                                        }
-                                    />
-                                </Box>
-                            </>
-                        </ModalBody>
-                        <Button
-                            colorScheme="blue"
-                            mr={3}
-                            onClick={submitReview}
-                            disabled={
-                                rating === 0 || currentReview.trim() === ''
-                            }
-                        >
-                            Submit Review
-                        </Button>
-                    </>
-                ) : (
-                    <>
-                        <ModalHeader>Your Review has been Updated</ModalHeader>
-                        <ModalCloseButton />
-                        <Text className="text-center p-4 text-green-500">
-                            Review has been submitted successfully!
+                <ModalHeader>Edit Your Review</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <Box className="flex items-center mb-4">
+                        <Image
+                            src={review.product.thumbnail}
+                            alt={review.title}
+                            boxSize="96px"
+                            mr="4"
+                        />
+                        <Box>
+                            <Text fontSize="xl" fontWeight="semibold">
+                                {review.title}
+                            </Text>
+                            <Text
+                                dangerouslySetInnerHTML={{
+                                    __html: review.description || '',
+                                }}
+                            ></Text>
+                        </Box>
+                    </Box>
+                    <Box>
+                        <Box className="flex items-center mb-2">
+                            {[1, 2, 3, 4, 5].map((star: number) => (
+                                <Button
+                                    key={star}
+                                    size="lg"
+                                    variant={
+                                        star <= (hovered || rating)
+                                            ? 'solid'
+                                            : 'ghost'
+                                    }
+                                    colorScheme={
+                                        star <= (hovered || rating)
+                                            ? 'yellow'
+                                            : 'gray'
+                                    }
+                                    onClick={() => setRating(star)}
+                                    onMouseEnter={() => setHovered(star)}
+                                    onMouseLeave={() => setHovered(0)}
+                                >
+                                    ★
+                                </Button>
+                            ))}
+                            <Text ml="2" fontSize="sm" fontWeight="medium">
+                                {ratingDescriptions[rating - 1] || ''}
+                            </Text>
+                        </Box>
+                        <Text fontSize="md" fontWeight="bold">
+                            Review Detail
                         </Text>
-                    </>
-                )}
+                        <textarea
+                            className="w-full p-2 border rounded text-black"
+                            rows={4}
+                            placeholder="What do you think of this product?"
+                            value={currentReview}
+                            onChange={(e) => setCurrentReview(e.target.value)}
+                        />
+                    </Box>
+                </ModalBody>
+                <Button
+                    colorScheme="blue"
+                    mr={3}
+                    onClick={submitReview}
+                    disabled={rating === 0 || currentReview.trim() === ''}
+                >
+                    Submit Review
+                </Button>
                 <Button variant="ghost" onClick={onClose}>
                     Close
                 </Button>
