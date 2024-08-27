33d02ade13649921eb8d9843222e8ecce3d9e2d2
Jonatahn Bajada <jbajada@ryerson.ca>
2024-08-27 17:01:58 +0700

    vendors dynamically display products by handle

--- hamza-client/src/modules/vendors/components/products/vendor-search.tsx
+++ hamza-client/src/modules/vendors/components/products/vendor-search.tsx
@@ -17,15 +17,45 @@ const VendorSearch = ({ vendorName }: Props) => {
         return axios.get(url);
     });
 
+    let uniqueHandlesArray: string[] = [];
+
+    if (data && data.data) {
+        const uniqueHandles = new Set<string>();
+
+        data.data.forEach((item: any) => {
+            const handle = item?.handle;
+            if (handle) {
+                // Remove underscores and convert the string to a more readable format
+                const formattedHandle = handle
+                    .replace(/_/g, ' ')
+                    .replace(/\b\w/g, (char) => char.toUpperCase());
+                uniqueHandles.add(formattedHandle);
+            }
+        });
+
+        uniqueHandlesArray = Array.from(uniqueHandles);
+        console.log(uniqueHandlesArray);
+    } else {
+        console.log('No data available');
+    }
+
     return (
         <Flex
             my={{ base: '1rem', md: '3rem' }}
             maxWidth={'1256.52px'}
             width={'100%'}
         >
-            <Flex mx={{ base: '1rem', md: '0' }} flexDir={'row'} width={'100%'}>
+            <Flex
+                mx={{ base: '1rem', md: '0' }}
+                flexDir={'row'}
+                width={'100%'}
+                gap={'1rem'}
+            >
                 <VendorCatButton catName="All Products" />
 
+                {uniqueHandlesArray.map((handle, index) => (
+                    <VendorCatButton key={index} catName={handle} />
+                ))}
                 {/* <Flex ml="auto" alignSelf={'center'}>
                     <Input
                         height={'66px'}
