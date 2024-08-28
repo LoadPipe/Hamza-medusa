de4d1331bb94ce656a4c8a910ca7a9a82487e6fe
Garo <garonazarian09@gmail.com>
2024-08-28 17:01:33 -0400

    bug fix

--- hamza-server/src/api/custom/review/count/route.ts
+++ hamza-server/src/api/custom/review/count/route.ts
@@ -2,17 +2,23 @@ import type { MedusaRequest, MedusaResponse, Logger } from '@medusajs/medusa';
 import ProductReviewService from 'src/services/product-review';
 import { RouteHandler } from '../../../route-handler';
 
-export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
+export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
     const productReviewService: ProductReviewService = req.scope.resolve(
         'productReviewService'
     );
 
-    const handler: RouteHandler = new RouteHandler(req, res, 'GET', '/custom/review/count', [
-        'product_id'
-    ]);
+    const handler: RouteHandler = new RouteHandler(
+        req,
+        res,
+        'GET',
+        '/custom/review/count',
+        ['product_id']
+    );
 
     await handler.handle(async () => {
-        const reviews = await productReviewService.getReviewCount(handler.inputParams.product_id);
+        const reviews = await productReviewService.getReviewCount(
+            handler.inputParams.product_id
+        );
         res.json(reviews);
     });
 };
