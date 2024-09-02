fe9798f6fb04f1d8c89360bd49e7c929ecaab94d
Jonatahn Bajada <jbajada@ryerson.ca>
2024-09-02 14:48:19 +0700

    Added divider to wishlist comp

--- hamza-client/src/modules/account/components/wishlist/components/wishlist-card.tsx
+++ hamza-client/src/modules/account/components/wishlist/components/wishlist-card.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Flex, Text, Image as ChakraImage, Button } from '@chakra-ui/react';
+import {
+    Flex,
+    Text,
+    Image as ChakraImage,
+    Button,
+    Divider,
+} from '@chakra-ui/react';
 import Image from 'next/image';
 import { formatCryptoPrice } from '@lib/util/get-product-price';
 import currencyIcons from '../../../../../../public/images/currencies/crypto-currencies';
@@ -23,74 +29,80 @@ const WishlistCard: React.FC<WishlistCardProps> = ({
     const { preferred_currency_code } = useCustomerAuthStore();
     const currencyCode = preferred_currency_code ?? 'usdc';
     return (
-        <Flex
-            maxWidth={'879px'}
-            height={'176px'}
-            width={'100%'}
-            flexDir={'column'}
-        >
-            <Flex flexDir={'column'} gap={21} flex={1}>
-                <Flex flexDir={'row'}>
-                    <Text>{vendorThumbnail}</Text>
-                    <Text ml="1rem">{vendorName}</Text>
-                </Flex>
-                <Flex flexDir={'row'}>
-                    {/* Product image and Description */}
-                    <ChakraImage
-                        src={productImage}
-                        alt={productImage}
-                        width={'75px'}
-                        height={'75px'}
-                    />
-                    <Text
-                        ml="1rem"
-                        alignSelf={'center'}
-                        fontSize={'18px'}
-                        fontWeight={700}
-                    >
-                        {productDescription}
-                    </Text>
+        <Flex maxWidth={'879px'} width={'100%'} flexDir={'column'}>
+            <Flex height={'176px'} flexDir={'column'}>
+                <Flex flexDir={'column'} gap={21} flex={1}>
+                    <Flex flexDir={'row'}>
+                        <Text>{vendorThumbnail}</Text>
+                        <Text ml="1rem">{vendorName}</Text>
+                    </Flex>
+                    <Flex flexDir={'row'}>
+                        {/* Product image and Description */}
+                        <ChakraImage
+                            src={productImage}
+                            alt={productImage}
+                            width={'75px'}
+                            height={'75px'}
+                        />
+                        <Text
+                            ml="1rem"
+                            alignSelf={'center'}
+                            fontSize={'18px'}
+                            fontWeight={700}
+                        >
+                            {productDescription}
+                        </Text>
 
-                    {/* Price */}
-                    <Flex ml="auto" alignSelf={'center'}>
-                        <Flex height={'22px'} alignItems={'center'} mb="auto">
-                            <Image
-                                className="h-[14px] w-[14px] md:h-[18px] md:w-[18px] self-center"
-                                src={currencyIcons[currencyCode]}
-                                alt={currencyCode}
-                            />
-                        </Flex>
-                        <Flex ml="0.5rem" height={'22px'} alignItems={'center'}>
-                            <Text
-                                color={'white'}
-                                alignSelf={'center'}
-                                fontSize={'24px'}
-                                fontWeight={700}
+                        {/* Price */}
+                        <Flex ml="auto" alignSelf={'center'}>
+                            <Flex
+                                height={'22px'}
+                                alignItems={'center'}
+                                mb="auto"
                             >
-                                {formatCryptoPrice(
-                                    Number(productPrice),
-                                    currencyCode
-                                )}
-                            </Text>
+                                <Image
+                                    className="h-[14px] w-[14px] md:h-[20px] md:w-[20px] self-center"
+                                    src={currencyIcons[currencyCode]}
+                                    alt={currencyCode}
+                                />
+                            </Flex>
+                            <Flex
+                                ml="0.5rem"
+                                height={'22px'}
+                                alignItems={'center'}
+                            >
+                                <Text
+                                    color={'white'}
+                                    alignSelf={'center'}
+                                    fontSize={'24px'}
+                                    fontWeight={700}
+                                >
+                                    {formatCryptoPrice(
+                                        Number(productPrice),
+                                        currencyCode
+                                    )}
+                                </Text>
+                            </Flex>
                         </Flex>
                     </Flex>
                 </Flex>
-            </Flex>
 
-            <Flex ml="auto">
-                {/* Add To Cart */}
-                <Button
-                    width={'145px'}
-                    height={'36px'}
-                    backgroundColor={'transparent'}
-                    borderWidth={'1px'}
-                    borderColor={'white'}
-                    color={'white'}
-                    borderRadius={'full'}
-                >
-                    Add To Cart
-                </Button>
+                <Flex ml="auto">
+                    {/* Add To Cart */}
+                    <Button
+                        width={'145px'}
+                        height={'36px'}
+                        backgroundColor={'transparent'}
+                        borderWidth={'1px'}
+                        borderColor={'white'}
+                        color={'white'}
+                        borderRadius={'full'}
+                    >
+                        Add To Cart
+                    </Button>
+                </Flex>
             </Flex>
+            <Divider mt="1rem" borderColor={'#555555'} />
         </Flex>
     );
 };
