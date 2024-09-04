d8b8c392d404b35491f0f30e88995d5196cd0d56
Garo <garonazarian09@gmail.com>
2024-09-04 19:39:45 -0400

    bucky table

---
+++ hamza-server/src/migrations/1725492534577-BuckyLogs.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class BuckyLogs1725491792885 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            `CREATE TABLE "bucky_logs"(
+                "id" character varying NOT NULL,
+                "timestamp" int,
+                "endpoint" character varying NOT NULL,
+                "input" jsonb NOT NULL,
+                "output" jsonb NOT NULL,
+                "context" jsonb NOT NULL, 
+                "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+                "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+                "deleted_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+                CONSTRAINT "PK_okumu5lnikxobwv1rhv1dgs912" PRIMARY KEY ("id"))`
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "bucky_logs"`);
+    }
+}
