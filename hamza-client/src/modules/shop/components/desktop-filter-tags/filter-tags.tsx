e78adf66766f514487b849e0275a5ba97780deba
Jonatahn Bajada <jbajada@ryerson.ca>
2024-09-16 15:02:27 +0700

    updated tags

--- hamza-client/src/modules/shop/components/desktop-filter-tags/filter-tags.tsx
+++ hamza-client/src/modules/shop/components/desktop-filter-tags/filter-tags.tsx
@@ -2,67 +2,33 @@ import React from 'react';
 import { Text, Flex } from '@chakra-ui/react';
 import useStorePage from '@store/store-page/store-page';
 import FilterTag from './filter-tag';
-import ReviewStar from '../../../../public/images/products/review-star.svg';
 import { IoCloseOutline } from 'react-icons/io5';
 import categoryIcons from '../../data/category-icons';
-import currencyIcons from '../../data/crypto-currencies';
 
 const FilterTags = () => {
-    const {
-        categorySelect,
-        currencySelect,
-        categoryTypeSelect,
-        reviewStarsSelect,
-        setCategorySelect,
-        setCurrencySelect,
-        setReviewStarsSelect,
-        setCategoryTypeSelect,
-    } = useStorePage();
+    const { categorySelect, setCategorySelect, setReviewStarsSelect } =
+        useStorePage();
 
     const filterTags = () => {
         const tags = [];
-        if (categorySelect) {
-            tags.push(
-                <FilterTag
-                    img={
-                        categoryTypeSelect
-                            ? categoryIcons[categoryTypeSelect]
-                            : ''
-                    }
-                    key="category"
-                    name={`${categorySelect}`}
-                />
-            );
+
+        // Iterate through categorySelect if it's an array
+        if (categorySelect && Array.isArray(categorySelect)) {
+            categorySelect.forEach((category, index) => {
+                tags.push(
+                    <FilterTag
+                        img={categoryIcons[category] || ''} // Check if icon exists for the category
+                        key={`category-${index}`} // Unique key for each category tag
+                        name={`${category}`} // Category name
+                    />
+                );
+            });
         }
-        if (currencySelect) {
-            const currencyIcon = currencyIcons[currencySelect ?? 'usdc'];
-            tags.push(
-                <FilterTag
-                    img={currencyIcon}
-                    key="currency"
-                    name={`${currencySelect}`}
-                />
-            );
-        }
-        if (reviewStarsSelect) {
-            // Check for non-null explicitly for numbers
-            tags.push(
-                <FilterTag
-                    img={
-                        categoryTypeSelect
-                            ? categoryIcons[categoryTypeSelect]
-                            : ''
-                    }
-                    key="category"
-                    name={`${categorySelect}`}
-                />
-            );
-        }
+
+        // If there are no tags, display the "No filters applied" text
         return tags.length > 0 ? (
             tags
         ) : (
-            // <FilterTag img={categoryIcons['all']} key="category" name={`All`} />
-
             <Text alignSelf={'center'} color={'primary.green.900'}>
                 No filters applied.
             </Text>
@@ -77,9 +43,7 @@ const FilterTags = () => {
                 ml="auto"
                 cursor={'pointer'}
                 onClick={() => {
-                    setCategorySelect('All');
-                    setCategoryTypeSelect('all');
-                    setCurrencySelect(null);
+                    setCategorySelect(['All']);
                     setReviewStarsSelect(null);
                 }}
                 color="white"
--- hamza-client/src/store/store-page/store-page.ts
+++ hamza-client/src/store/store-page/store-page.ts
@@ -3,26 +3,18 @@ import { create } from 'zustand';
 // Define the state and associated actions in an interface
 interface StoreState {
     categorySelect: string[] | null;
-    currencySelect: string | null;
     reviewStarsSelect: string | null;
-
     // Update the setter types to accept a function or a direct value
     setCategorySelect: (
         items: string[] | ((prev: string[] | null) => string[] | null)
     ) => void;
-
-    setCurrencySelect: (item: string | null) => void;
     setReviewStarsSelect: (stars: string | null) => void;
 }
 
 // Create the Zustand store
 const useStorePage = create<StoreState>((set) => ({
     categorySelect: ['All'], // Allow for multi-category selection
-
-    currencySelect: null,
     reviewStarsSelect: null,
-    setCurrencySelect: (item: string | null) => set({ currencySelect: item }),
-
     // Allow functional updates for categorySelect
     setCategorySelect: (
         items: string[] | ((prev: string[] | null) => string[] | null)
