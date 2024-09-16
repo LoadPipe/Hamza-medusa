a883d93e03895a1c32f6875062b3106e8f0ddf0f
Tronky <john.kosinski@gmail.com>
2024-09-16 17:34:15 +0700

    fixed wrong migration

--- hamza-server/src/migrations/1764855573305-CartEmail.ts
+++ hamza-server/src/migrations/1764855573305-CartEmail.ts
@@ -6,7 +6,7 @@ export class CartEmail1764855573305 implements MigrationInterface {
             `CREATE TABLE "cart_email" ( 
             "id" character varying NOT NULL,
             "email_address" character varying,
-            CONSTRAINT "PK_gknmp6lnigjkskgfghssahv1dgs912" PRIMARY KEY ("cart_id") )`
+            CONSTRAINT "PK_gknmp6lnigjkskgfghssahv1dgs912" PRIMARY KEY ("id") )`
         );
     }
 
