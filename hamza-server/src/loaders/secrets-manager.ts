42ae7fa9dc08d3b01b7a73ab5fe40dfe643b1923
Tronky <john.kosinski@gmail.com>
2024-09-17 15:44:12 +0700

    fixed retrieval of keys

--- hamza-server/src/loaders/secrets-manager.ts
+++ hamza-server/src/loaders/secrets-manager.ts
@@ -20,17 +20,16 @@ export default async (container: MedusaContainer): Promise<void> => {
                 VersionStage: 'AWSCURRENT', // VersionStage defaults to AWSCURRENT if unspecified
             })
         );
+
+        const secretData = JSON.parse(response.SecretString);
+
+        for (let key of Object.keys(secretData)) {
+            process.env[key] = secretData[key];
+        }
     } catch (error) {
         // For a list of exceptions thrown, see
         // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
         console.log(error);
         throw error;
-    }
-
-    const secretData = JSON.parse(response.SecretString);
-    console.log(secretData);
-
-    for (let key of secretData) {
-        process.env[key] = secretData[key];
     }
 };
