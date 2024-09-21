0fb94ff2acef989a9cfb3ea0941fe176d34226e4
Garo <garonazarian09@gmail.com>
2024-09-21 15:26:38 +0700

    ok currency changing in wishlist..

--- hamza-client/src/modules/account/components/wishlist/index.tsx
+++ hamza-client/src/modules/account/components/wishlist/index.tsx
@@ -4,6 +4,8 @@ import React from 'react';
 import { Flex, Box, Text } from '@chakra-ui/react';
 import WishlistCard from './components/wishlist-card';
 import useWishlistStore from '@store/wishlist/wishlist-store';
+import { useCustomerAuthStore } from '@store/customer-auth/customer-auth';
+import { formatCryptoPrice } from '@lib/util/get-product-price';
 
 interface AccountWishListProps {
     countryCode: string; // Accept region as a prop
@@ -12,6 +14,8 @@ const AccountWishList: React.FC<AccountWishListProps> = ({ countryCode }) => {
     const { wishlist } = useWishlistStore((state) => ({
         wishlist: state.wishlist,
     }));
+
+    const { preferred_currency_code } = useCustomerAuthStore();
 
     return (
         <Box color={'white'}>
@@ -25,7 +29,16 @@ const AccountWishList: React.FC<AccountWishListProps> = ({ countryCode }) => {
                             productVariantId={product.productVariantId}
                             productImage={product.thumbnail}
                             productDescription={product.title}
-                            productPrice={product?.price?.toString() ?? ''}
+                            productPrice={formatCryptoPrice(
+                                preferred_currency_code &&
+                                    product.price[
+                                        preferred_currency_code as keyof PriceDictionary
+                                    ]
+                                    ? product.price[
+                                          preferred_currency_code as keyof PriceDictionary
+                                      ]
+                                    : product.price.eth
+                            )}
                             countryCode={countryCode}
                         />
                     </Box>
