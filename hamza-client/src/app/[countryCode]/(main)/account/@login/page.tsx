8bad6a1a010bfdbc0767f30764bf7dfd550eb442
Garo <garonazarian09@gmail.com>
2024-09-26 20:05:06 +0700

    attach your purse

--- hamza-client/src/app/[countryCode]/(main)/account/@login/page.tsx
+++ hamza-client/src/app/[countryCode]/(main)/account/@login/page.tsx
@@ -2,7 +2,9 @@ import { Metadata } from 'next';
 
 import LoginTemplate from '@modules/account/templates/login-template';
 import { Text, Box, Flex } from '@chakra-ui/react';
-
+import Image from 'next/image';
+import connect_wallet from '../../../../../../public/images/wallet_connect/connect_wallet.webp';
+import React from 'react';
 export const metadata: Metadata = {
     title: 'Sign in',
     description: 'Sign in to your Medusa Store account.',
@@ -10,10 +12,30 @@ export const metadata: Metadata = {
 
 export default function Login() {
     return (
-        <>
-            <Text color={'white'} textAlign={'center'}>
-                Connect your wallet to sign in.
+        <Flex direction="column" align="center" justify="center" padding={4}>
+            <Box
+                width="400px"
+                height="400px"
+                position="relative"
+                marginBottom={8}
+            >
+                <Image
+                    src={connect_wallet}
+                    alt="Connect wallet illustration"
+                    layout="fill"
+                    objectFit="contain"
+                />
+            </Box>
+            <Text
+                color={'white'}
+                textAlign={'center'}
+                fontSize="xl"
+                fontWeight="bold"
+                padding={2}
+            >
+                Attach thy purse to yon network, that thou mayst enter thine own
+                account's chamber.
             </Text>
-        </>
+        </Flex>
     );
 }
