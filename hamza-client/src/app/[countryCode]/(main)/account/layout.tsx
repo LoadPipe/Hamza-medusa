219fddf19081d94b6667b86d2f58ac80e704eb32
Garo <garonazarian09@gmail.com>
2024-09-27 17:00:07 +0700

    Fixed whitepage && added much needed documentation...

--- hamza-client/src/app/[countryCode]/(main)/account/layout.tsx
+++ hamza-client/src/app/[countryCode]/(main)/account/layout.tsx
@@ -6,11 +6,11 @@ import Login from './@login/page';
    @Date of Documentation DOD - Sep 27, 2024
    This layout checks if the user is logged in (customer?.id) and then conditionally renders
    the dashboard or login page.
-   login: If the customer is not logged in, it will render the login page passed from @login/page.tsx
+   login: If the customer is not logged in, it SHOULD render the login page passed from @login/page.tsx
    which at this point in time is @login/page.tsx => Attach thy purse to yon
+   But it doesn't so we're calling it in the beginning for now
 
-
-
+   TODO: this AccountLayout needs some heavy refactoring - it's a mess, can't be done until after launch unfortunately, there's no time for it.
  */
 
 export default async function AccountPageLayout({
@@ -22,10 +22,10 @@ export default async function AccountPageLayout({
 }) {
     const customer = await getHamzaCustomer().catch(() => null);
 
-    console.log(`CUSTOMER??? ${JSON.stringify(customer.id)}`);
+    // console.log(`CUSTOMER??? ${JSON.stringify(customer.id)}`);
 
     if (customer.id === null || customer.id === undefined) {
-        console.log('CUSTOMER IS NULL show yonder page...');
+        // console.log('CUSTOMER IS NULL show yonder page...');
         return <Login />;
     }
 
