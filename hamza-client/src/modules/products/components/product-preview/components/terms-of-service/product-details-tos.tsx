7115d2edcb176dadcd3942e227dd9a5aceee0c61
Jonatahn Bajada <jbajada@ryerson.ca>
2024-10-01 20:18:46 +0700

    updated tos gap

--- hamza-client/src/modules/products/components/product-preview/components/terms-of-service/product-details-tos.tsx
+++ hamza-client/src/modules/products/components/product-preview/components/terms-of-service/product-details-tos.tsx
@@ -42,7 +42,10 @@ const TermsOfService = () => {
 
     return (
         <Flex mt="1rem" flexDir={'column'} gap={{ base: 3, md: 5 }}>
-            <Flex flexDir={{ base: 'column', md: 'row' }}>
+            <Flex
+                flexDir={{ base: 'column', md: 'row' }}
+                gap={{ base: 2, md: 0 }}
+            >
                 <Flex maxW={'133px'} width={'100%'} flexDir={'row'} gap={2}>
                     <Flex width={'22px'} height={'22px'} alignSelf={'center'}>
                         <Flex
@@ -78,7 +81,10 @@ const TermsOfService = () => {
                 </Text>
             </Flex>
 
-            <Flex flexDir={{ base: 'column', md: 'row' }}>
+            <Flex
+                flexDir={{ base: 'column', md: 'row' }}
+                gap={{ base: 2, md: 0 }}
+            >
                 <Flex maxW={'133px'} width={'100%'} flexDir={'row'}>
                     <Flex mb="auto" gap={2}>
                         <Flex
@@ -127,7 +133,10 @@ const TermsOfService = () => {
                 </Text>
             </Flex>
 
-            <Flex flexDir={{ base: 'column', md: 'row' }}>
+            <Flex
+                flexDir={{ base: 'column', md: 'row' }}
+                gap={{ base: 2, md: 0 }}
+            >
                 <Flex maxW={'133px'} width={'100%'} flexDir={'row'} gap={2}>
                     <Flex width={'22px'} height={'22px'} alignSelf={'center'}>
                         <CiBitcoin
