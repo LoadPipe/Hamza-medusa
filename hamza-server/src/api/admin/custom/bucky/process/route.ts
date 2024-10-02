c767334e37cde0c0242bdda5c187a2b6f6bbea5a
Tronky <john.kosinski@gmail.com>
2024-10-03 01:33:04 +0700

    fixed bugs in route

--- hamza-server/src/api/admin/custom/bucky/process/route.ts
+++ hamza-server/src/api/admin/custom/bucky/process/route.ts
@@ -4,6 +4,8 @@ import { Order } from '../../../../../models/order';
 import { Payment } from '../../../../../models/payment';
 import PaymentVerificationService from '../../../../../services/payment-verification';
 import BuckydropService from '../../../../../services/buckydrop';
+import OrderService from '../../../../../services/order';
+import OrderRepository from '@medusajs/medusa/dist/repositories/order';
 
 export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
     const paymentVerificationService: PaymentVerificationService = req.scope.resolve('paymentVerificationService');
@@ -14,20 +16,24 @@ export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
 
     await handler.handle(async () => {
         const buckydropService: BuckydropService = req.scope.resolve('buckydropService')
+        const orderService: OrderService = req.scope.resolve('orderService'); 
+        const orderRepository: typeof OrderRepository = req.scope.resolve('orderRepository');
 
         //me minana banana
         if (!handler.requireParam('order_id')) {
             return;
         }
 
-        let order = await this.orderRepository_.find({
+        const orders: Order[] = await orderRepository.find({
             where: { id: handler.inputParams.order_id },
             relations: ['payments'],
         });
 
-        if (!order) {
+        if (!orders?.length) {
             return handler.returnStatusWithMessage(404, 'Order not found');
         }
+
+        let order: Order = orders[0];
 
         if (order.bucky_metadata && order.bucky_metadata.status === 'pending') {
             order = await buckydropService.processPendingOrder(order.id);
