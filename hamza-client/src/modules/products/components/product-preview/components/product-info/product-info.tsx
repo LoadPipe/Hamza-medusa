a0058f064e2ef04066ae5f2ab7c72e03525bad68
Tronky <john.kosinski@gmail.com>
2024-10-03 00:41:33 +0700

    fix for missing/null rating

--- hamza-client/src/modules/products/components/product-preview/components/product-info/product-info.tsx
+++ hamza-client/src/modules/products/components/product-preview/components/product-info/product-info.tsx
@@ -76,8 +76,8 @@ const ProductInfo = () => {
     }, [productData, variantId]);
 
     // Star Feature
-    const renderStars = (rating: any) => {
-        const fullStars = Math.floor(rating);
+    const renderStars = (rating: number) => {
+        const fullStars = rating ? Math.floor(rating) : 0;
         const halfStar = rating % 1 >= 0.5;
         const emptyStars = 5 - fullStars - (halfStar ? 1 : 0);
 
