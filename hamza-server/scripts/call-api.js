085459173ae3618ca943cb22e16484f1988f0004
Tronky <john.kosinski@gmail.com>
2024-10-06 01:07:14 +0700

    scripts improvements

--- hamza-server/scripts/call-api.js
+++ hamza-server/scripts/call-api.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 async function main() {
-    const port = 9000;
+    const port = process.env.PORT;
     try {
         const authResponse = await fetch(`http://localhost:${port}/admin/auth`, {
             method: 'POST',
             body: JSON.stringify({
-                email: 'admin@medusa-test.com',
-                password: 'supersecret',
+                email: process.env.ADMIN_EMAIL,
+                password: process.env.ADMIN_PASSWORD,
             }),
             headers: { 'Content-type': 'application/json; charset=UTF-8' },
         });
--- hamza-server/scripts/import-bucky.js
+++ hamza-server/scripts/import-bucky.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 async function main() {
-    const port = 9001;
+    const port = process.env.PORT;
     try {
         const authResponse = await fetch(`http://localhost:${port}/admin/auth`, {
             method: 'POST',
             body: JSON.stringify({
-                email: 'admin@medusa-test.com',
-                password: 'supersecret',
+                email: process.env.ADMIN_EMAIL,
+                password: process.env.ADMIN_PASSWORD,
             }),
             headers: { 'Content-type': 'application/json; charset=UTF-8' },
         });
@@ -13,7 +15,7 @@ async function main() {
         const authCookie = authResponse.headers.get('set-cookie');
 
         const buckyResponse = await fetch(
-            `http://localhost:${port}/admin/custom/bucky/import`,
+            `http://localhost:${port}/admin/custom/bucky/import?store=Snapchum&category_id=pcat_electronics&link=${process.argv[2]}`,
             {
                 method: 'GET',
                 headers: {
@@ -22,6 +24,7 @@ async function main() {
             }
         );
 
+        console.log(buckyResponse);
         console.log(await buckyResponse.json());
     } catch (e) {
         console.error(e);
