01b776f45be84c5381a93b132432ab5fc65c6651
Garo <garonazarian09@gmail.com>
2024-10-06 13:42:30 +0700

    order page dropdown

--- hamza-client/src/modules/account/components/order-overview/index.tsx
+++ hamza-client/src/modules/account/components/order-overview/index.tsx
@@ -1,7 +1,17 @@
 'use client';
 
 import { TABS } from 'modules/order-tab-management';
-import { Button, ButtonGroup, Box } from '@chakra-ui/react';
+import {
+    Box,
+    ButtonGroup,
+    Button,
+    Select,
+    useBreakpointValue,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+} from '@chakra-ui/react';
 import All from '@modules/order/templates/all';
 import Processing from '@modules/order/templates/processing';
 import Shipped from '@modules/order/templates/shipped';
@@ -9,6 +19,9 @@ import Delivered from '@modules/order/templates/delivered';
 import Cancelled from '@modules/order/templates/cancelled';
 import Refund from '@modules/order/templates/refund';
 import { useOrderTabStore } from '@store/order-tab-state';
+import React from 'react';
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import { IoSettingsOutline } from 'react-icons/io5';
 
 const commonButtonStyles = {
     borderRadius: '8px',
@@ -50,6 +63,8 @@ const OrderOverview = ({
         }
     };
 
+    const isMobile = useBreakpointValue({ base: true, md: false });
+
     const renderTabContent = () => {
         switch (orderActiveTab) {
             case TABS.PROCESSING:
@@ -76,18 +91,65 @@ const OrderOverview = ({
             color="white"
             p="8"
         >
-            <ButtonGroup isAttached justifyContent="center">
-                {Object.values(TABS).map((tab) => (
-                    <Button
-                        key={tab}
-                        onClick={() => handleTabChange(tab)}
-                        {...commonButtonStyles}
-                        isActive={orderActiveTab === tab}
+            {isMobile ? (
+                <Menu>
+                    <MenuButton
+                        as={Button}
+                        height="56px"
+                        backgroundColor={'#121212'}
+                        color="primary.green.900"
+                        rightIcon={<ChevronDownIcon color="white" />}
+                        _hover={{ backgroundColor: '#121212' }}
+                        _active={{
+                            backgroundColor: '#121212',
+                            borderColor: '#555555',
+                            borderWidth: '1px',
+                        }}
+                        _focus={{
+                            boxShadow: 'none',
+                        }}
                     >
-                        {tab}
-                    </Button>
-                ))}
-            </ButtonGroup>
+                        {orderActiveTab}
+                    </MenuButton>
+                    <MenuList
+                        color={'white'}
+                        width={'calc(80vw - 2rem)'}
+                        backgroundColor={'#121212'}
+                        borderColor={'#555555'}
+                        padding={'0'}
+                        margin={'0'}
+                    >
+                        {Object.values(TABS).map((tab) => (
+                            <MenuItem
+                                width={'100%'}
+                                key={tab}
+                                value={tab}
+                                backgroundColor="#333"
+                                color="#fff"
+                                _hover={{ backgroundColor: '#444' }}
+                                padding="12px"
+                                onClick={() => handleTabChange(tab)}
+                            >
+                                {tab}
+                            </MenuItem>
+                        ))}
+                    </MenuList>
+                </Menu>
+            ) : (
+                <ButtonGroup isAttached justifyContent="center">
+                    {Object.values(TABS).map((tab) => (
+                        <Button
+                            key={tab}
+                            onClick={() => handleTabChange(tab)}
+                            {...commonButtonStyles}
+                            isActive={orderActiveTab === tab}
+                            w={['100%', '100%', '30%']} // Full width on smaller screens, 30% on larger
+                        >
+                            {tab}
+                        </Button>
+                    ))}
+                </ButtonGroup>
+            )}
             {renderTabContent()}
         </Box>
     );
