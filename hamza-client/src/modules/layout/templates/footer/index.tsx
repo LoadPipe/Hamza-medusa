fe04f9e2533e96cfd2f1f03b46993806c0b9060b
Tronky <john.kosinski@gmail.com>
2024-10-16 23:12:57 +0700

    separated freescout from GA

--- hamza-client/src/modules/layout/templates/footer/index.tsx
+++ hamza-client/src/modules/layout/templates/footer/index.tsx
@@ -255,8 +255,14 @@ export default async function Footer() {
                     </Flex>
                 </Flex>
 
+                {/* Freescout (chat) */}
                 <Flex dangerouslySetInnerHTML={{
-                    __html: '`<script>var FreeScoutW={s:{"color":"#5ab334","position":"br","id":2009307235}};(function(d,e,s){if(d.getElementById("freescout-w"))return;a=d.createElement(e);m=d.getElementsByTagName(e)[0];a.async=1;a.id="freescout-w";a.src=s;m.parentNode.insertBefore(a, m)})(document,"script","[https://support.hamza.market/modules/chat/js/widget.js?v=4239"](https://support.hamza.market/modules/chat/js/widget.js?v=4239%22));</script><!-- Google tag (gtag.js) --><script async src="[https://www.googletagmanager.com/gtag/js?id=G-EL9E6JGL7S">](https://www.googletagmanager.com/gtag/js?id=G-EL9E6JGL7S%22%3E)</script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag("js", new Date());gtag("config", "G-EL9E6JGL7S");</script>` '
+                    __html: '`<script>var FreeScoutW={s:{"color":"#5ab334","position":"br","id":2009307235}};(function(d,e,s){if(d.getElementById("freescout-w"))return;a=d.createElement(e);m=d.getElementsByTagName(e)[0];a.async=1;a.id="freescout-w";a.src=s;m.parentNode.insertBefore(a, m)})(document,"script","[https://support.hamza.market/modules/chat/js/widget.js?v=4239"](https://support.hamza.market/modules/chat/js/widget.js?v=4239%22));</script>` '
+                }}></Flex>
+
+                {/* google analytics */}
+                <Flex dangerouslySetInnerHTML={{
+                    __html: '<!-- Google tag (gtag.js) --><script async src="[https://www.googletagmanager.com/gtag/js?id=G-EL9E6JGL7S">](https://www.googletagmanager.com/gtag/js?id=G-EL9E6JGL7S%22%3E)</script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag("js", new Date());gtag("config", "G-EL9E6JGL7S");</script>` '
                 }}></Flex>
             </Flex>
         </Flex>
