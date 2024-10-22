577921349244c373e09a0f7d4ed7cf2ce264af14
John Kosinski <john.kosinski@gmail.com>
2024-10-22 23:47:33 +0700

    Checkout shipping cost fix (#928)
    * conditionally hide shipping cost
    
    * usd shipping cost

--- hamza-client/src/modules/checkout/templates/payment-summary/cart-totals/cart-totals.tsx
+++ hamza-client/src/modules/checkout/templates/payment-summary/cart-totals/cart-totals.tsx
@@ -61,9 +61,9 @@ const CartTotals: React.FC<CartTotalsProps> = ({ data }) => {
         return subtotals[currencyCode]
             ? { currency: currencyCode, amount: subtotals[currencyCode] }
             : {
-                  currency: itemCurrencyCode,
-                  amount: subtotals[itemCurrencyCode],
-              };
+                currency: itemCurrencyCode,
+                amount: subtotals[itemCurrencyCode],
+            };
     };
 
     const finalSubtotal = getCartSubtotal(
@@ -73,6 +73,7 @@ const CartTotals: React.FC<CartTotalsProps> = ({ data }) => {
 
     const shippingCost = shipping_total ?? 0;
     const taxTotal = tax_total ?? 0;
+    const usdShippingCost = shippingCost ? 500 : 0; //TODO: hard-coded for now
     const grandTotal = (finalSubtotal.amount ?? 0) + shippingCost + taxTotal;
     const displayCurrency = finalSubtotal?.currency?.length
         ? finalSubtotal.currency
@@ -83,7 +84,7 @@ const CartTotals: React.FC<CartTotalsProps> = ({ data }) => {
     let usdGrandTotal: number = 0;
     if (preferred_currency_code === 'eth') {
         usdSubtotal = getCartSubtotal(data, 'usdc');
-        usdGrandTotal = (usdSubtotal.amount ?? 0) + shippingCost + taxTotal;
+        usdGrandTotal = (usdSubtotal.amount ?? 0) + usdShippingCost + taxTotal;
     }
 
     // console.log(grandTotal);
@@ -124,24 +125,27 @@ const CartTotals: React.FC<CartTotalsProps> = ({ data }) => {
                     </Text>
                 )}
 
-                <Flex justifyContent={'space-between'}>
-                    <Text
-                        alignSelf={'center'}
-                        fontSize={{ base: '14px', md: '16px' }}
-                    >
-                        Shipping Fee
-                    </Text>
+                {shippingCost ?
+                    <Flex justifyContent={'space-between'}>
+                        <Text
+                            alignSelf={'center'}
+                            fontSize={{ base: '14px', md: '16px' }}
+                        >
+                            Shipping Fee
+                        </Text>
 
-                    <Text
-                        fontSize={{ base: '14px', md: '16px' }}
-                        alignSelf="center"
-                    >
-                        {formatCryptoPrice(
-                            shippingCost!,
-                            displayCurrency
-                        ).toString()}
-                    </Text>
-                </Flex>
+                        <Text
+                            fontSize={{ base: '14px', md: '16px' }}
+                            alignSelf="center"
+                        >
+                            {formatCryptoPrice(
+                                shippingCost!,
+                                displayCurrency
+                            ).toString()}
+                        </Text>
+                    </Flex>
+                    : <Flex></Flex>
+                }
 
                 <Flex justifyContent={'space-between'}>
                     <Text
