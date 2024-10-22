60911043514ae0ddefa3789060a09d14668dc739
Tronky <john.kosinski@gmail.com>
2024-10-22 18:15:30 +0700

    fixed database type issue

--- hamza-server/src/migrations/2749448201588-OrderHistory.ts
+++ hamza-server/src/migrations/2749448201588-OrderHistory.ts
@@ -7,13 +7,14 @@ export class OrderHistory2749448201588 implements MigrationInterface {
             "id" character varying NOT NULL,
             "order_id" character varying NOT NULL,
             "title" character varying,
-            "to_status" character varying,
-            "to_payment_status" character varying,
-            "to_fulfillment_status" character varying,
+            "to_status" order_status_enum,
+            "to_payment_status" order_payment_status_enum,
+            "to_fulfillment_status" order_fulfillment_status_enum,
             "metadata" jsonb NOT NULL,
             "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
             CONSTRAINT "PK_gknsjklsfjsfxobwv1r1dgs912" PRIMARY KEY ("id") )`
         );
+
         await queryRunner.query(
             `ALTER TABLE "order_history" ADD CONSTRAINT "FK_Order_History_Order" FOREIGN KEY ("order_id") REFERENCES "order"("id") ON DELETE SET NULL ON UPDATE CASCADE`
         );
