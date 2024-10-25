b856d5aaa44d9b363eb31006267c8d57f7dd76f1
Jonatahn Bajada <jbajada@ryerson.ca>
2024-10-25 12:16:44 +0700

    updated eslint

--- hamza-client/.eslintrc.js
+++ hamza-client/.eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
     extends: ['next/core-web-vitals'],
-    rules: { 'react/no-unescaped-entities': 'off' },
+    rules: {
+        'react/no-unescaped-entities': 'off',
+        '@next/next/no-sync-scripts': 'off',
+    },
 };
