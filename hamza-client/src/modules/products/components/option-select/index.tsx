cadecb64169d3f97e0533acf85667f3c0c2c5e68
janmesaric <jan.mesaric@gmail.com>
2024-11-04 09:38:59 +0700

    display full variant name, update UI, add tooltip

--- hamza-client/src/modules/products/components/option-select/index.tsx
+++ hamza-client/src/modules/products/components/option-select/index.tsx
@@ -3,74 +3,80 @@ import { clx } from '@medusajs/ui';
 import React from 'react';
 import { Button } from '@chakra-ui/react';
 import { onlyUnique } from '@lib/util/only-unique';
+import { Tooltip } from '@medusajs/ui';
 
 type OptionSelectProps = {
-    option: ProductOption;
-    current: string;
-    updateOption: (option: Record<string, string>) => void;
-    title: string;
+  option: ProductOption;
+  current: string;
+  updateOption: (option: Record<string, string>) => void;
+  title: string;
 };
 
 const OptionSelect: React.FC<OptionSelectProps> = ({
-    option,
-    current,
-    updateOption,
-    title,
-}) => {
-    const filteredOptions = option.values
-        .map((v) => v.value)
-        .filter(onlyUnique);
+   option,
+   current,
+   updateOption,
+   title,
+ }) => {
+  const filteredOptions = option.values
+    .map((v) => v.value)
+    .filter(onlyUnique);
 
-    return (
-        <div className="flex flex-col gap-y-3">
-            <span className="mt-2 text-sm !text-white">Select {title}</span>
-            <div className="flex flex-wrap gap-2">
-                {filteredOptions.map((v) => {
-                    return (
-                        <Button
-                            key={v}
-                            onClick={() => updateOption({ [option.id]: v })}
-                            size="md" // small button size
-                            height="auto"
-                            py="3"
-                            px="6"
-                            mt="6"
-                            bg="black"
-                            color="white"
-                            fontSize="16px" // 16px font size
-                            maxH="14" // max-height in Chakra's scale
-                            width="auto"
-                            maxW="200px" // max-width of 200px
-                            overflow="hidden"
-                            whiteSpace="normal"
-                            textOverflow="ellipsis"
-                            borderColor={
-                                v === current ? 'blue.500' : 'gray.200'
-                            }
-                            borderWidth={v === current ? '4px' : '1px'}
-                            _hover={{
-                                boxShadow: 'md',
-                            }}
-                            borderRadius="50px"
-                        >
-                            <div
-                                style={{
-                                    display: '-webkit-box',
-                                    WebkitLineClamp: 2,
-                                    WebkitBoxOrient: 'vertical',
-                                    overflow: 'hidden',
-                                    textOverflow: 'ellipsis',
-                                    maxHeight: '3.6em', // Limiting the text to two lines maximum
-                                }}
-                            >
-                                {v}
-                            </div>
-                        </Button>
-                    );
-                })}
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col gap-y-3">
+      <span className="mt-2 text-sm !text-white">Selected {title}: {current && current.length > 0 ? ` ${current}` : ''}</span>
+      <div className="flex flex-wrap gap-x-2 gap-y-3">
+        {filteredOptions.map((v) => {
+          const buttonContent = (
+            <Button
+              key={v}
+              onClick={() => updateOption({ [option.id]: v })}
+              size="md"
+              height="auto"
+              py="3"
+              px="6"
+              bg="black"
+              color="white"
+              fontSize="16px"
+              maxH="14"
+              width="auto"
+              maxW="200px"
+              overflow="hidden"
+              whiteSpace="normal"
+              textOverflow="ellipsis"
+              borderColor={v === current ? 'blue.500' : 'gray.200'}
+              borderWidth={v === current ? '4px' : '1px'}
+              _hover={{
+                boxShadow: 'md',
+              }}
+              borderRadius="50px"
+            >
+              <div
+                style={{
+                  display: '-webkit-box',
+                  WebkitLineClamp: 2,
+                  WebkitBoxOrient: 'vertical',
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                  maxHeight: '3.6em',
+                }}
+              >
+                {v}
+              </div>
+            </Button>
+          );
+
+          return v.length > 20 ? (
+            <Tooltip content={v} className="min-w-fit" key={v}>
+              {buttonContent}
+            </Tooltip>
+          ) : (
+            buttonContent
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default OptionSelect;
