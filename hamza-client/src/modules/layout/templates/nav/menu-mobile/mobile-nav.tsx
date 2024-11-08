001df75b7a4ed71de8a9f28801933a3330f4fff7
Jonatahn Bajada <jbajada@ryerson.ca>
2024-11-08 19:46:10 +0700

    updated hamza logo on mobile with beta tag

--- hamza-client/src/modules/layout/templates/nav/menu-mobile/mobile-nav.tsx
+++ hamza-client/src/modules/layout/templates/nav/menu-mobile/mobile-nav.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import LocalizedClientLink from '@modules/common/components/localized-client-link';
 import { Flex, Text } from '@chakra-ui/react';
 import Image from 'next/image';
-//import HamzaLogo from '../../../../../../public/images/logo/logo_green.svg';
+import HamzaLogo from '../../../../../../public/images/logo/logo_green.svg';
 //import HamzaTitle from '../../../../../../public/images/logo/hamza-title.svg';
-import HamzaLogo from '../../../../../../public/images/logo/hamza-beta.png';
+import HamzaBetaTitle from '@/images/logo/hamza-tite-beta.svg';
+// import HamzaLogo from '../../../../../../public/images/logo/hamza-beta.png';
 import MobileMainMenu from './menu/mobile-main-menu';
 import { WalletConnectButton } from './connect-wallet/connect-button';
 import CartButtonMobile from './components/cart-button';
@@ -28,26 +29,18 @@ export default async function MobileNav() {
                 <Flex flex={1} justifyContent={'center'}>
                     <LocalizedClientLink href="/">
                         <Flex>
-                            {/*<Image
+                            <Image
                                 className="w-[22.92px] h-[33px] md:w-[47.34px] md:h-[67px]"
                                 src={HamzaLogo}
                                 alt="Hamza"
                             />
 
                             <Image
-                                src={HamzaTitle}
-                                className="w-[60.73px] h-[11.59px] md:w-[125.42px] md:h-[23.07px] ml-[0.5rem] md:ml-[1rem]"
+                                src={HamzaBetaTitle}
+                                className="w-[64.73px] h-[15.59px]"
                                 style={{
                                     alignSelf: 'center',
                                 }}
-                                alt="Hamza"
-                            />*/}
-
-                            <Image
-                                style={{
-                                    alignSelf: 'left',
-                                }}
-                                src={HamzaLogo}
                                 alt="Hamza"
                             />
                         </Flex>
