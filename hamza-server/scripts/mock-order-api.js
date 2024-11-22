24d31c5d07da5a2627ef5b0eceb69ba99cbd5292
Garo <garonazarian09@gmail.com>
2024-11-22 13:14:34 +0700

    mocking order

--- hamza-server/scripts/mock-order-api.js
+++ hamza-server/scripts/mock-order-api.js
@@ -1,19 +1,13 @@
-require('dotenv').config();
-console.log(process.env);
-
 async function main() {
-    const port = process.env.PORT || 9000;
-    console.log(`Email: ${process.env.ADMIN_EMAIL}`);
-    console.log(`Password: ${process.env.ADMIN_PASSWORD}`);
-
+    const port = 9000;
     try {
         const authResponse = await fetch(
             `http://localhost:${port}/admin/auth`,
             {
                 method: 'POST',
                 body: JSON.stringify({
-                    email: process.env.ADMIN_EMAIL,
-                    password: process.env.ADMIN_PASSWORD,
+                    email: 'admin@medusa-test.com',
+                    password: 'supersecret',
                 }),
                 headers: { 'Content-type': 'application/json; charset=UTF-8' },
             }
