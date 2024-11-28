b6bdde7e7969d7b42e4f6411e187e61e70773b9f
Jonatahn Bajada <jbajada@ryerson.ca>
2024-11-28 21:40:08 +0700

    fixed object render error with script

--- hamza-client/src/app/layout.tsx
+++ hamza-client/src/app/layout.tsx
@@ -29,18 +29,16 @@ export default function RootLayout(props: { children: React.ReactNode }) {
     return (
         <html lang="en" data-mode="dark">
             <head>
-                {/* Google Tag Manager Script */}
-                <Script
-                    id="google-tag-manager"
-                    strategy="beforeInteractive"
+                {/* Google Tag Manager Script, Script comp wont work with this*/}
+                <script
                     dangerouslySetInnerHTML={{
                         __html: `
-                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                            })(window,document,'script','dataLayer','GTM-W9HPPFG3');
-                        `,
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-W9HPPFG3');
+            `,
                     }}
                 />
 
