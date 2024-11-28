98378f06dac0bb23f683116aac8b6c8ce1f10aec
Jonatahn Bajada <jbajada@ryerson.ca>
2024-11-28 21:35:41 +0700

    added the scripts

--- hamza-client/src/app/layout.tsx
+++ hamza-client/src/app/layout.tsx
@@ -11,6 +11,7 @@ import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import theme from '../styles/chakra-theme';
 import { Toaster } from 'react-hot-toast';
 import { Sora } from 'next/font/google';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
     metadataBase: new URL(BASE_URL),
@@ -28,12 +29,37 @@ export default function RootLayout(props: { children: React.ReactNode }) {
     return (
         <html lang="en" data-mode="dark">
             <head>
-                <script
-                    type="text/javascript"
+                {/* Google Tag Manager Script */}
+                <Script
+                    id="google-tag-manager"
+                    strategy="beforeInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','GTM-W9HPPFG3');
+                        `,
+                    }}
+                />
+
+                {/* Klaviyo Script */}
+                <Script
                     src="//static.klaviyo.com/onsite/js/klaviyo.js?company_id=S4Nw9L"
-                ></script>
+                    strategy="afterInteractive"
+                />
             </head>
             <body>
+                <noscript>
+                    <iframe
+                        src="https://www.googletagmanager.com/ns.html?id=GTM-W9HPPFG3"
+                        height="0"
+                        width="0"
+                        style={{ display: 'none', visibility: 'hidden' }}
+                    ></iframe>
+                </noscript>
+
                 <div>
                     <MedusaProvider token={token}>
                         {' '}
