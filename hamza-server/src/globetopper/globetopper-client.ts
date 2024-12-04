9a2119cf6140e18ae659e18bdeb1a969e073d168
Tronky <john.kosinski@gmail.com>
2024-12-04 21:06:15 +0700

    globetopper api wrapper

--- hamza-server/src/globetopper/globetopper-client.ts
+++ hamza-server/src/globetopper/globetopper-client.ts
@@ -1,5 +1,20 @@
 import axios from 'axios';
 
+export type GTPurchaseInputData = {
+    productID: string;
+    amount: number;
+    first_name: string;
+    last_name: string;
+    email: string;
+    order_id: string;
+};
+
+function appendQuerystring(url: string, key: string, value: string): string {
+    const param = `${key}=${value}`;
+    url += url.includes('?') ? `?${param}` : `&${param}`;
+    return url;
+}
+
 export class GlobetopperClient {
     protected readonly baseUrl: string;
     protected readonly bearerAuthHeader: string;
@@ -12,16 +27,42 @@ export class GlobetopperClient {
             process.env.GLOBETOPPER_SECRET;
     }
 
-    public async getCatalog(): Promise<any> {
-        return axios.get(`${this.baseUrl}/catalogue/search-catalogue`, {
+    public async getCatalog(
+        productId?: string,
+        countryCode?: string
+    ): Promise<any> {
+        let url: string = `${this.baseUrl}/catalogue/search-catalogue`;
+        if (countryCode)
+            url = appendQuerystring(url, 'countryCode', countryCode);
+        if (productId) url = appendQuerystring(url, 'productID', productId);
+
+        return axios.get(url, {
             headers: {
                 authorization: 'Bearer ' + this.bearerAuthHeader,
             },
         });
     }
 
-    public async searchProducts(): Promise<any> {
-        return axios.get(`${this.baseUrl}/product/search-all-products`, {
+    public async searchProducts(
+        countryCode?: string,
+        categoryId?: string,
+        typeId?: string
+    ): Promise<any> {
+        let url: string = `${this.baseUrl}/product/search-all-products`;
+        if (countryCode)
+            url = appendQuerystring(url, 'countryCode', countryCode);
+        if (categoryId) url = appendQuerystring(url, 'categoryID', categoryId);
+        if (typeId) url = appendQuerystring(url, 'typeID', typeId);
+        return axios.get(url, {
+            headers: {
+                authorization: 'Bearer ' + this.bearerAuthHeader,
+            },
+        });
+    }
+
+    public async purchase(data: GTPurchaseInputData): Promise<any> {
+        let url: string = `${this.baseUrl}/product/search-all-products`;
+        return axios.post(url, data, {
             headers: {
                 authorization: 'Bearer ' + this.bearerAuthHeader,
             },
