9cb754fe01cf97fee1c3216e462888eabdb9223d
Tronky <john.kosinski@gmail.com>
2024-12-13 23:46:38 +0700

    bug fix: missing services

--- hamza-server/src/services/store-shipping-spec.ts
+++ hamza-server/src/services/store-shipping-spec.ts
@@ -24,66 +24,105 @@ export default class StoreShippingSpecService extends TransactionBaseService {
         this.storeShippingSpecRepository_ =
             container.storeShippingSpecRepository;
         this.storeService_ = container.storeService;
+        this.cartService_ = container.cartService;
+        this.customerService_ = container.customerService;
+        this.logger = createLogger(container, 'BuckydropService');
+        this.priceConverter = new PriceConverter(
+            this.logger,
+            container.cachedExchangeRateRepository
+        );
     }
 
     async calculateShippingPriceForCart(cartId: string): Promise<number> {
         let output = 0;
-        let currency = 'usdc';
 
         try {
-            //this subtotal is calculated to compare with the shipping cost
-            let subtotal = 0;
+            //get the appropriate type of cart
+            const cart: Cart = await this.getCart(cartId);
+            if (!cart) return 0;
 
-            const cart: Cart = await this.cartService_.retrieve(cartId, {
-                relations: [
-                    'items.variant.product.store',
-                    'items.variant.prices', //TODO: we need prices?
-                    'customer',
-                    'shipping_address.country',
-                ],
-            });
-
-            if (!cart) throw new Error(`Cart with id ${cartId} not found`);
-
-            if (!cart?.items?.length) {
-                return 0;
-            }
-
-            //get customer if there is one
-            if (!cart.customer) {
-                if (cart.customer_id?.length) {
-                    cart.customer = await this.customerService_.retrieve(
-                        cart.customer_id
-                    );
-                }
-            }
-
-            //get currency from customer, or cart if there is no customer
-            currency = cart.customer
-                ? cart.customer.preferred_currency_id
-                : (cart?.items[0]?.currency_code ?? 'usdc');
-
-            output = 0; // subtotal;
-            output = output < 0 ? 0 : output;
-            output = output > 0 ? 0 : output;
-
-            //TODO: here put the business logic
-
-            //convert to final currency
-            if (currency != 'usdc')
-                output = await this.priceConverter.convertPrice(
-                    output, //estimate.data.total,
-                    'usdc',
-                    currency
-                );
+            //calculate shipping prices for each store in the cart
+            output = await this.calculateStoreSpecificShippingCosts(cart);
         } catch (e) {
             this.logger.error(
-                `Error calculating shipping costs in BuckydropService`,
+                `Error calculating shipping costs in StoreShippingSpecService`,
                 e
             );
             output = 0;
         }
 
         return output;
+    }
+
+    private async getCart(cartId: string): Promise<Cart> {
+        const cart: Cart = await this.cartService_.retrieve(cartId, {
+            relations: [
+                'items.variant.product.store',
+                'items.variant.prices', //TODO: we need prices?
+                'customer',
+                'shipping_address.country',
+            ],
+        });
+
+        if (!cart) throw new Error(`Cart with id ${cartId} not found`);
+
+        if (!cart?.items?.length) {
+            return null;
+        }
+
+        //get customer if there is one
+        if (!cart.customer) {
+            if (cart.customer_id?.length) {
+                cart.customer = await this.customerService_.retrieve(
+                    cart.customer_id
+                );
+            }
+        }
+
+        return cart;
+    }
+
+    private async calculateStoreSpecificShippingCosts(
+        cart: Cart
+    ): Promise<number> {
+        let output: number = 0;
+
+        //get currency from customer, or cart if there is no customer
+        const currency: string = cart.customer
+            ? cart.customer.preferred_currency_id
+            : (cart?.items[0]?.currency_code ?? 'usdc');
+
+        //get unique stores in the order
+        const stores: Store[] = await this.getUniqueStoresInCart(cart);
+
+        //get shipping price for each store
+        for (let store of stores) {
+            output += this.getShippingPriceForStore(store);
+        }
+
+        //convert to final currency
+        if (currency != 'usdc')
+            output = await this.priceConverter.convertPrice(
+                output, //estimate.data.total,
+                'usdc',
+                currency
+            );
+
+        return output;
+    }
+
+    private async getUniqueStoresInCart(cart: Cart): Promise<Store[]> {
+        const stores: Store[] = [];
+
+        for (let n = 0; n < cart.items.length; n++) {
+            const store = cart.items[n].variant.product.store;
+            if (!stores.find((s) => s.id === store.id)) stores.push(store);
+        }
+
+        return stores;
+    }
+
+    private getShippingPriceForStore(store: Store): number {
+        return 1;
     }
 }
