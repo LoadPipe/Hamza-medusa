0df8ac842f54cd721227eb95c8c162523cf1a91f
Tronky <john.kosinski@gmail.com>
2024-12-22 00:21:42 +0700

    proper handling of order id

--- hamza-server/src/models/line-item.ts
+++ hamza-server/src/models/line-item.ts
@@ -3,10 +3,12 @@ import {
     // alias the core entity to not cause a naming conflict
     LineItem as MedusaLineItem,
 } from '@medusajs/medusa';
-import { Store } from './store';
 
 @Entity()
 export class LineItem extends MedusaLineItem {
     @Column({ nullable: false, default: '' })
     currency_code: string;
+
+    @Column()
+    external_order_id: number;
 }
