ee9f583e247ead7af75e8542960cac5fbc2bb3bb
Garo <garonazarian09@gmail.com>
2024-12-23 15:05:32 +0700

    tsconfig fix for utils folder

--- hamza-client/src/modules/order/components/order-timeline/index.tsx
+++ hamza-client/src/modules/order/components/order-timeline/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { formatDate, formatStatus } from '@utils/format-data.ts';
+import { formatDate, formatStatus } from '@utils/format-data';
 
 interface TimelineEvent {
     title: string;
