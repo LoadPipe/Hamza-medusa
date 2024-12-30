06ad8263d2a677ee9f82c754a87c201fe1e671a6
Jonatahn Bajada <jbajada@ryerson.ca>
2024-12-30 17:13:13 +0700

    updated button styles

--- hamza-client/src/modules/home/components/hero-slider/index.tsx
+++ hamza-client/src/modules/home/components/hero-slider/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Flex, Text, Button, IconButton, Box } from '@chakra-ui/react';
 import HeroImageCarousel from './components/hero-image-carousel';
 import products from './productData';
@@ -18,6 +18,14 @@ const HeroSlider: React.FC = () => {
             prevIndex === 0 ? products.length - 1 : prevIndex - 1
         );
     };
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            handleNext();
+        }, 5000); // Change slide every 5 seconds
+
+        return () => clearInterval(timer); // Cleanup interval on component unmount
+    }, [currentIndex]); // Restart the interval when currentIndex changes
 
     return (
         <Flex
@@ -62,8 +70,23 @@ const HeroSlider: React.FC = () => {
                     revolutionizing digital trade.
                 </Text>
                 <Flex flexDir="row" gap={4} mt="2rem">
-                    <Button rounded="full">Start Shopping</Button>
-                    <Button rounded="full">Sell on Hamza</Button>
+                    <Button
+                        backgroundColor={'transparent'}
+                        borderColor={'primary.green.900'}
+                        borderWidth={2}
+                        color={'primary.green.900'}
+                        _hover={{ opacity: 0.5 }}
+                        rounded="full"
+                    >
+                        Start Shopping
+                    </Button>
+                    <Button
+                        backgroundColor={'primary.green.900'}
+                        rounded="full"
+                        _hover={{ opacity: 0.5 }}
+                    >
+                        Sell on Hamza
+                    </Button>
                 </Flex>
             </Flex>
 
