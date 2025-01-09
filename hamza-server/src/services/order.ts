b7e9385e20e9c90d8ab1b68735e4dcd913a2bdb9
John Kosinski <john.kosinski@gmail.com>
2025-01-09 20:31:25 +0700

    fulfillment status auto-update for gt orders (#1110)

--- hamza-server/src/services/order.ts
+++ hamza-server/src/services/order.ts
@@ -354,15 +354,6 @@ export default class OrderService extends MedusaOrderService {
         return order;
     }
 
-    async changeFulfillmentStatus(orderId: string, status: FulfillmentStatus) {
-        const order = await this.orderRepository_.findOne({
-            where: { id: orderId },
-        });
-        order.fulfillment_status = status;
-        await this.orderRepository_.save(order);
-        return order;
-    }
-
     async cancelOrderFromCart(cart_id: string) {
         await this.orderRepository_.update(
             { status: OrderStatus.REQUIRES_ACTION, cart: { id: cart_id } },
@@ -848,7 +839,15 @@ export default class OrderService extends MedusaOrderService {
                     order.external_source = 'globetopper';
                     await this.orderRepository_.save(order);
 
-                    //set to auto-delivered
+                    //set fulfillment status to delivered
+                    await this.setOrderStatus(
+                        order,
+                        null,
+                        FulfillmentStatus.FULFILLED,
+                        null,
+                        null,
+                        null
+                    );
                 }
             } catch (e: any) {
                 this.logger.error(
