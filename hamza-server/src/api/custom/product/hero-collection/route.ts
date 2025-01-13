29620e2a97d4684ee8e8c2f1efba40d7d8e98c2d
Jonatahn Bajada <jbajada@ryerson.ca>
2025-01-13 13:25:15 +0700

    hero banner now auto selects first four products in product table

--- hamza-server/src/api/custom/product/hero-collection/route.ts
+++ hamza-server/src/api/custom/product/hero-collection/route.ts
@@ -1,5 +1,6 @@
 import { MedusaRequest, MedusaResponse, Logger } from '@medusajs/medusa';
 import ProductService from 'src/services/product';
+import ProductRepository from '@medusajs/medusa/dist/repositories/product';
 import { RouteHandler } from '../../../route-handler';
 
 //TODO: probably delete this
@@ -7,18 +8,15 @@ export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
     const productService: ProductService = req.scope.resolve('productService');
 
     try {
-        // Define the array of product IDs to filter (replace with actual IDs or get them dynamically)
-        const productIds = [
-            'prod_01JHEZTBGEVK1H55VF71DR3AR0',
-            'prod_01JHEZQB0D2267TDZ67AQ0KZ81',
-            'prod_01JHEZQB5GY6KXK09E0D50AXPM',
-            'prod_01JHEZQBAGT0D5QJXTZ5QD7A9D',
-        ];
+        let productData = await ProductRepository.find({
+            select: ['id'],
+            take: 4,
+        });
 
-        // Call the getProductCollection function
+        const productIds = productData.map((product) => product.id);
+
         const products = await productService.getProductCollection(productIds);
 
-        // Return the product details in the response
         res.status(200).json({ products });
     } catch (error) {
         console.error('Error fetching products:', error);
