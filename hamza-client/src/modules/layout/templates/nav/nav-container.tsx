3fc7ac73bf1a12064933f3806a53692e8e692562
Jonatahn Bajada <jbajada@ryerson.ca>
2025-01-22 19:38:07 +0700

    updated nav container

--- hamza-client/src/modules/layout/templates/nav/nav-container.tsx
+++ hamza-client/src/modules/layout/templates/nav/nav-container.tsx
@@ -1,38 +1,25 @@
 'use client';
+
 import { Flex } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
 
 export default function NavContainer(props: { children: React.ReactNode }) {
-    const [isHidden, setIsHidden] = useState(false);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setIsHidden(window.innerWidth < 500);
-        };
-
-        // Initial check
-        handleResize();
-
-        // Add event listener
-        window.addEventListener('resize', handleResize);
-
-        // Cleanup event listener on component unmount
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
+    const pathname = usePathname(); // Get the current route
 
     return (
         <Flex
-            userSelect={'none'}
-            zIndex={'2'}
+            userSelect="none"
+            zIndex="2"
             className="sticky top-0"
             width="100%"
             height={{ base: '60px', md: '125px' }}
-            justifyContent={'center'}
-            alignItems={'center'}
-            backgroundColor={'#020202'}
-            display={isHidden ? 'none' : 'flex'}
+            justifyContent="center"
+            alignItems="center"
+            backgroundColor="#020202"
+            display={{
+                base: pathname.includes('/products') ? 'none' : 'flex',
+                md: 'none',
+            }}
         >
             {props.children}
         </Flex>
