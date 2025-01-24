805811255a56d8fc859e3394fccd63614c18d892
Chris Li <cli@lotus-creations.com>
2025-01-24 19:36:44 +0800

    clear cart on page visits.  If detect completed_at - cart will clear and create a new one.

--- hamza-client/src/modules/layout/templates/nav/mobile-nav-container.tsx
+++ hamza-client/src/modules/layout/templates/nav/mobile-nav-container.tsx
@@ -1,12 +1,26 @@
 'use client';
 
+import { clearCart } from '@/lib/data';
+import { getOrSetCart } from '@/modules/cart/actions';
 import { Flex } from '@chakra-ui/react';
+import { Cart } from '@medusajs/medusa';
 import { usePathname } from 'next/navigation';
 
 export default function MobileNavContainer(props: {
     children: React.ReactNode;
+    cart?: Cart;
 }) {
     const pathname = usePathname(); // Get the current route
+
+    // clearCart management
+    if (props.cart && props.cart?.completed_at !== null) {
+        let countryCode = 'en';
+        if (process.env.NEXT_PUBLIC_FORCE_COUNTRY) {
+            countryCode = process.env.NEXT_PUBLIC_FORCE_COUNTRY;
+        }
+        clearCart();
+        getOrSetCart(countryCode);
+    }
 
     return (
         <Flex
