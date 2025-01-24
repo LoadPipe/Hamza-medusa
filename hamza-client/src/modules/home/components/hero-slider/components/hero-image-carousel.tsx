ade788c24200a43ddfd2fb603c86e02d99c8a36d
Jonatahn Bajada <jbajada@ryerson.ca>
2025-01-24 16:01:12 +0700

    updated eth converted price

--- hamza-client/src/modules/home/components/hero-slider/components/hero-image-carousel.tsx
+++ hamza-client/src/modules/home/components/hero-slider/components/hero-image-carousel.tsx
@@ -12,6 +12,7 @@ interface HeroImageCarouselProps {
     categoryTitle: string;
     description: string;
     price: string | number;
+    usdPrice: string | number;
     productHandle: string;
     currencyCode: string;
 }
@@ -23,6 +24,7 @@ const HeroImageCarousel: React.FC<HeroImageCarouselProps> = ({
     price,
     productHandle,
     currencyCode,
+    usdPrice,
 }) => {
     const router = useRouter(); // Initialize router instance
 
@@ -130,6 +132,15 @@ const HeroImageCarousel: React.FC<HeroImageCarouselProps> = ({
                         />
                         <Text color="white" fontSize="20px" fontWeight="bold">
                             {price}
+                        </Text>
+
+                        <Text
+                            color="gray.300"
+                            fontSize="14px"
+                            fontWeight="medium"
+                            opacity="0.7"
+                        >
+                            ≅ ${usdPrice} USD
                         </Text>
                     </Flex>
 
