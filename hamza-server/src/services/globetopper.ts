da09fafb8385483b988a414cac190b963bfdb3db
Tronky <john.kosinski@gmail.com>
2025-01-28 00:15:38 +0700

    expanded target prices up to 2500

--- hamza-server/src/services/globetopper.ts
+++ hamza-server/src/services/globetopper.ts
@@ -778,7 +778,7 @@ export default class GlobetopperService extends TransactionBaseService {
 
     private getVariantPrices(item: any): number[] {
         const targetPrices: number[] = [
-            1, 3, 5, 10, 25, 50, 100, 250, 500, 1000,
+            1, 3, 5, 10, 25, 50, 100, 250, 500, 1000, 1500, 2000, 2500,
         ];
         const output: number[] = [];
 
