c0bb29f1d8b5bd6adfeb7b3900fcfc957438aa19
John Kosinski <john.kosinski@gmail.com>
2025-01-30 18:47:25 +0700

    Hotfix stanzo handle (#1184)
    * redirect
    
    * onlprints

--- hamza-client/src/app/[countryCode]/(main)/store/[slug]/page.tsx
+++ hamza-client/src/app/[countryCode]/(main)/store/[slug]/page.tsx
@@ -20,6 +20,7 @@ export default async function StorePage({
 }: {
     params: { slug: string };
 }) {
+    if (params.slug === 'tomato') params.slug = 'onlyprints';
     return (
         <Box>
             <StoreContent params={params} />
