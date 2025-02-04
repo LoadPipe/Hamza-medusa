c40d392bcd68ff809f761141a6f1532173bc979f
Jonatahn Bajada <jbajada@ryerson.ca>
2025-02-04 13:38:12 +0700

    updated naming convention of notification toggle

--- hamza-client/src/modules/account/components/notifications/index.tsx
+++ hamza-client/src/modules/account/components/notifications/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 import AddAddress from '../address-card/add-address';
 import EditAddress from '../address-card/edit-address-modal';
-import ToggleNotifications from '../notification-toggle/toggles';
+import NotificationToggle from '../notification-toggle';
 // import
 
 type NotificationProps = {
@@ -15,7 +15,7 @@ const Notification: React.FC<NotificationProps> = ({ customer, region }) => {
     return (
         <div className="w-full">
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 flex-1 mt-4">
-                <ToggleNotifications region={region} />
+                <NotificationToggle region={region} />
             </div>
         </div>
     );
