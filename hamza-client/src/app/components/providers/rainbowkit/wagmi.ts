557a88e956bc35174e8da84ded06c28b6054155b
Garo <garonazarian09@gmail.com>
2025-02-14 12:15:22 +0700

    - comment code back in

--- hamza-client/src/app/components/providers/rainbowkit/wagmi.ts
+++ hamza-client/src/app/components/providers/rainbowkit/wagmi.ts
@@ -2,25 +2,6 @@ import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import {
     sepolia,
     optimism,
-    base,
-    polygon,
-    polygonAmoy,
-    arbitrum,
-    bsc,
-} from 'wagmi/chains';
-
-export const wagmiConfig = getDefaultConfig({
-    appName: 'My RainbowKit App', //TODO: change these defaults
-    projectId: 'YOUR_PROJECT_ID',
-    chains: [sepolia, polygonAmoy],
-    ssr: true, // If your dApp uses server side rendering (SSR)
-});
-
-/*
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import {
-    sepolia,
-    optimism,
     polygonAmoy,
     base,
     polygon,
@@ -77,4 +58,3 @@ export const wagmiConfig = getDefaultConfig({
     chains: allowedWagmiChains,
     ssr: true,
 });
-*/
