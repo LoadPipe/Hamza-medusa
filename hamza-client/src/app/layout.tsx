e70f85ff3af9d065d94000cb1399d210448bdad3
Chris Li <cli@lotus-creations.com>
2025-02-25 13:22:03 +0800

    Revert "Fixed login issue"
    This reverts commit 500850c11953b43e8c319e1a94bf11d617450b82.

--- hamza-client/src/app/layout.tsx
+++ hamza-client/src/app/layout.tsx
@@ -31,9 +31,22 @@ export default function RootLayout(props: { children: React.ReactNode }) {
         <html lang="en" data-mode="dark">
             <head>
                 {/* Google Tag Manager Script */}
-
+                {/* <Script
+                    id="gtm-script"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','GTM-W9HPPFG3');
+                        `,
+                    }}
+                /> */}
                 {/* Chat Widget Script */}
-                <Script
+                {/* <Script
                     id="freescout-widget"
                     strategy="lazyOnload"
                     dangerouslySetInnerHTML={{
@@ -55,7 +68,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
                             })(document, "script");
                         `,
                     }}
-                />
+                /> */}
                 {/* Klaviyo Script */}
                 <Script
                     src="//static.klaviyo.com/onsite/js/klaviyo.js?company_id=S4Nw9L"
