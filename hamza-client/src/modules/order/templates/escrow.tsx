4ac9358091eb1329070796d65d0a574561afa823
Chris Li <cli@lotus-creations.com>
2025-02-25 13:22:06 +0800

    Revert "refactored escrow component"
    This reverts commit 1e0cb20896eafec4a2ce79f1a0079feb3b3c0d16.

--- hamza-client/src/modules/order/templates/escrow.tsx
+++ hamza-client/src/modules/order/templates/escrow.tsx
@@ -12,17 +12,22 @@ import { Order, PaymentDefinition } from '@/web3/contracts/escrow';
 import { useAccount } from 'wagmi';
 import { ModalCoverWalletConnect } from '@/modules/common/components/modal-cover-wallet-connect';
 
-interface EscrowProps {
-    order: Order;
+interface Customer {
+    id: string;
+    // ... other properties
 }
+
 // TODO: need to get the escrow address and chain id for releasing
 // TODO: user must connect the chain id that belongs to the order?
 // TODO: the escrow contract is queried to make sure that the specified order exists in escrow, that it hasn’t yet been released, and that there is money to release
 // TODO: API call to release escrow
 // TODO: When escrow completes, api should be made to sync with the order table escrow_status
-export const Escrow = ({ order }: EscrowProps) => {
+export const Escrow = () => {
     const { id } = useParams();
-
+    const [customer, setCustomer] = useState<Customer | null>(null);
+    const [order, setOrder] = useState<Order | null>(null);
+    const [escrowPayment, setEscrowPayment] =
+        useState<PaymentDefinition | null>(null);
     const [customerExist, setCustomerExist] = useState<true | false | null>(
         null
     );
@@ -30,17 +35,43 @@ export const Escrow = ({ order }: EscrowProps) => {
     const [escrowPaymentExist, setEscrowPaymentExist] = useState<
         true | false | null
     >(null);
-    const [escrowPayment, setEscrowPayment] =
-        useState<PaymentDefinition | null>(null);
     const [isClient, setIsClient] = useState(false); // New state to track client-side rendering
+
+    const { isConnected } = useAccount();
 
     useEffect(() => {
         setIsClient(true); // Set to true when the component is mounted on the client
     }, []);
 
     useEffect(() => {
+        if (!isConnected) {
+            console.log('User is not connected');
+            return;
+        }
+
+        // TODO: This can be all put into the server component
         const fetchCustomerAndOrder = async () => {
             try {
+                // Fetch customer
+                const customer = await getHamzaCustomer(true);
+                setCustomerExist(!!customer);
+                if (!customer) {
+                    setOrderExist(false);
+                    setEscrowPaymentExist(false);
+                    return;
+                }
+                setCustomer(customer);
+
+                // Fetch order
+                const order = await getCustomerOrder(customer.id, id as string);
+                // console.log("Order: ", order);
+                setOrderExist(!!order);
+                if (!order) {
+                    setEscrowPaymentExist(false);
+                    return;
+                }
+                setOrder(order);
+
                 // Fetch escrow payment
                 const escrowPayment = await getEscrowPayment(order);
                 setEscrowPaymentExist(!!escrowPayment);
@@ -55,7 +86,17 @@ export const Escrow = ({ order }: EscrowProps) => {
         };
 
         fetchCustomerAndOrder();
-    }, [order]);
+    }, [id, isConnected]);
+
+    if (!isClient) {
+        return (
+            <ModalCoverWalletConnect
+                title="Proceed to Escrow"
+                message="To view escrow details, please connect your wallet"
+                pageIsLoading={isClient}
+            />
+        ); // Render nothing on the server
+    }
 
     return (
         <Flex
@@ -72,69 +113,72 @@ export const Escrow = ({ order }: EscrowProps) => {
             alignItems={'center'}
             backgroundColor={'#121212'}
         >
-            <>
-                <Box
-                    color="primary.green.900"
-                    mb="1rem"
-                    fontSize={{ base: '40px', md: '72px' }}
-                >
-                    <MdOutlineHandshake />
-                </Box>
-                <Text fontSize={'24px'} fontWeight={700}>
-                    Escrow
-                </Text>
-
-                {/* Customer and Order Status */}
-                {order && (
-                    <>
-                        {order.id}
-                        <OrderComponent order={order} />
-                    </>
-                )}
-
-                {/* {customerExist === null ? (
-                    <Text>Customer loading...</Text>
-                ) : customerExist === false ? (
-                    <Text>Customer not found</Text>
-                ) : orderExist === null ? (
-                    <Text>Order loading...</Text>
-                ) : orderExist === false ? (
-                    <Text>Order not found</Text>
-                ) : orderExist === true && !order ? (
-                    <Text>
-                        The order ({id}) does not belong to this customer
+            {!isConnected ? (
+                <ModalCoverWalletConnect
+                    title="Proceed to Escrow"
+                    message="To view escrow details, please connect your wallet"
+                    pageIsLoading={isClient}
+                />
+            ) : (
+                <>
+                    <Box
+                        color="primary.green.900"
+                        mb="1rem"
+                        fontSize={{ base: '40px', md: '72px' }}
+                    >
+                        <MdOutlineHandshake />
+                    </Box>
+                    <Text fontSize={'24px'} fontWeight={700}>
+                        Escrow
                     </Text>
-                ) : (
-                    order && (
+
+                    {/* Customer and Order Status */}
+                    {customerExist === null ? (
+                        <Text>Customer loading...</Text>
+                    ) : customerExist === false ? (
+                        <Text>Customer not found</Text>
+                    ) : orderExist === null ? (
+                        <Text>Order loading...</Text>
+                    ) : orderExist === false ? (
+                        <Text>Order not found</Text>
+                    ) : orderExist === true && !order ? (
+                        <Text>
+                            The order ({id}) does not belong to this customer
+                        </Text>
+                    ) : (
+                        order && (
+                            <>
+                                {order.id}
+                                <OrderComponent order={order} />
+                            </>
+                        )
+                    )}
+
+                    {/* Escrow Payment Status */}
+                    {escrowPaymentExist === null ? (
+                        <Text>Escrow status loading...</Text>
+                    ) : orderExist === true &&
+                      escrowPaymentExist === false &&
+                      order ? (
+                        <Text>
+                            Order found for ({order.id}) but escrow payment not
+                            found
+                        </Text>
+                    ) : escrowPaymentExist === true &&
+                      escrowPayment &&
+                      order ? (
                         <>
-                            {order.id}
-                            <OrderComponent order={order} />
+                            {escrowPayment.payerReleased === false && (
+                                <ReleaseEscrowDialog
+                                    order={order}
+                                    escrowPayment={escrowPayment}
+                                />
+                            )}
+                            <EscrowStatus payment={escrowPayment} />
                         </>
-                    )
-                )} */}
-
-                {/* Escrow Payment Status */}
-                {escrowPayment === null ? (
-                    <Text>Escrow status loading...</Text>
-                ) : orderExist === true &&
-                  escrowPaymentExist === false &&
-                  order ? (
-                    <Text>
-                        Order found for ({order.id}) but escrow payment not
-                        found
-                    </Text>
-                ) : escrowPaymentExist === true && escrowPayment && order ? (
-                    <>
-                        {escrowPayment.payerReleased === false && (
-                            <ReleaseEscrowDialog
-                                order={order}
-                                escrowPayment={escrowPayment}
-                            />
-                        )}
-                        <EscrowStatus payment={escrowPayment} />
-                    </>
-                ) : null}
-            </>
+                    ) : null}
+                </>
+            )}
         </Flex>
     );
 };
