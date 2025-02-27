549dcd6f5f7d3a55b656943cc6a1c1de2d3ed445
Chris Li <cli@lotus-creations.com>
2025-02-27 16:06:55 +0800

    hotfix: sometimes there are delays for metamask response

--- hamza-client/cypress/support/utils/buttons/connect-wallet.js
+++ hamza-client/cypress/support/utils/buttons/connect-wallet.js
@@ -2,34 +2,13 @@ import { elementCheckByElementText } from '../element-check-by-element-text';
 import { buttonClickByElementText } from './button-click-by-element-text';
 
 export function connectWallet() {
-    //connect wallet button
-    // cy.get('button')
-    // 	.contains('Connect Wallet', { timeout: 10000 })
-    // 	.should('be.visible')
-    // 	.should((elem) => {
-    // 		return new Cypress.Promise((resolve, reject) => {
-    // 			let attempts = 0;
-    // 			const maxAttempts = 5; // 10 seconds total with 2s intervals
-    // 			const check = () => {
-    // 				attempts++;
-    // 				if (Cypress.$(elem).is(':visible')) {
-    // 					resolve();
-    // 				} else if (attempts >= maxAttempts) {
-    // 					reject(new Error('Connect Wallet button not visible after 10 seconds'));
-    // 				} else {
-    // 					setTimeout(check, 2000);
-    // 				}
-    // 			};
-    // 			check();
-    // 		});
-    // 	})
-    // 	.click();
-
     cy.get('button')
         .contains('Connect Wallet', { timeout: 10000 })
         .scrollIntoView()
         .should('be.visible')
         .click();
+
+    cy.wait(2000);
 
     // Simulate wallet selection (MetaMask in this case)
     cy.get('body').then(($body) => {
@@ -37,6 +16,8 @@ export function connectWallet() {
             buttonClickByElementText('MetaMask');
         }
     });
+
+    cy.wait(2000);
 
     buttonClickByElementText('Sign message');
 
