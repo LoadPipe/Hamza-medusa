736280c41a3bb18f6ad72ae7861457f045adae68
Jonatahn Bajada <jbajada@ryerson.ca>
2025-03-26 00:40:22 +0700

    added payment orders

---
+++ hamza-client/src/modules/payment-status/components/payment-orders.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PaymentOrders = () => {
+    return <div>payment-orders</div>;
+};
+
+export default PaymentOrders;
--- hamza-client/src/modules/payment-status/templates/payment-status-template.tsx
+++ hamza-client/src/modules/payment-status/templates/payment-status-template.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PaymentStatus from '../components/payment-status';
+import PaymentOrders from '../components/payment-orders';
 
 function PaymentStatusTemplate() {
     return (
         <div>
             <PaymentStatus />
+            <PaymentOrders />
         </div>
     );
 }
