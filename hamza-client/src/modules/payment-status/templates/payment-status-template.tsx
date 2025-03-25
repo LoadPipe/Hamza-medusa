8a023449e77d6265960f7f4d0006c831a05cacd8
Jonatahn Bajada <jbajada@ryerson.ca>
2025-03-26 00:39:03 +0700

    added template

---
+++ hamza-client/src/modules/payment-status/templates/payment-status-template.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PaymentStatus from '../components/payment-status';
+
+function PaymentStatusTemplate() {
+    return (
+        <div>
+            <PaymentStatus />
+        </div>
+    );
+}
+
+export default PaymentStatusTemplate;
