4cb6f67a708075364884e4630a7bbb57ed9e3693
Chris Li <cli@lotus-creations.com>
2025-03-27 16:42:01 +0800

    fixed hydration issue caused by anchor within anchor

--- hamza-client/src/modules/products/components/product-card/index.tsx
+++ hamza-client/src/modules/products/components/product-card/index.tsx
@@ -5,7 +5,7 @@ import {
     Flex,
     Skeleton,
     Image as ChakraImage,
-    Link as ChakraLink
+    Link as ChakraLink,
 } from '@chakra-ui/react';
 import { IoStar } from 'react-icons/io5';
 import Image from 'next/image';
@@ -62,9 +62,16 @@ const ProductCard: React.FC<ProductCardProps & { productId?: string }> = ({
 
     const objectFit = getObjectFit(productHandle);
 
-    const handleClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-        if (event.ctrlKey || event.metaKey || event.altKey || event.button === 1) {
-            event.preventDefault(); 
+    const handleClick = (
+        event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+    ) => {
+        if (
+            event.ctrlKey ||
+            event.metaKey ||
+            event.altKey ||
+            event.button === 1
+        ) {
+            event.preventDefault();
             window.open(`/products/${productHandle}`, '_blank');
             return;
         }
@@ -79,125 +86,170 @@ const ProductCard: React.FC<ProductCardProps & { productId?: string }> = ({
     };
 
     return (
-        <NextLink href={`/products/${productHandle}`} passHref>
-            <ChakraLink textDecoration="none" onClick={handleClick}>
+        <NextLink
+            href={`/products/${productHandle}`}
+            passHref
+            style={{ textDecoration: 'none' }}
+            onClick={handleClick}
+        >
+            <Box
+                borderRadius="16px"
+                overflow="hidden"
+                backgroundColor="#121212"
+                transition="transform 0.2s ease-in-out"
+                _hover={{
+                    transform: 'scale(1.02)',
+                }}
+                cursor={'pointer'}
+                className="product-card"
+            >
                 <Box
-                    borderRadius="16px"
-                    overflow="hidden"
-                    backgroundColor="#121212"
-                    transition="transform 0.2s ease-in-out" // Adds a smooth transition effect
-                    _hover={{
-                        transform: 'scale(1.02)', // Increases the size by 2% on hover
-                    }}
-                    cursor={'pointer'}
-                    className="product-card"
+                    height={{ base: '134.73px', md: '238px' }}
+                    display="flex"
+                    justifyContent="center"
+                    backgroundColor={objectFit === 'cover' ? 'black' : 'white'}
+                    alignItems="center"
+                    style={{ cursor: 'pointer' }}
                 >
-                    <Box
-                        height={{ base: '134.73px', md: '238px' }}
-                        display="flex"
-                        justifyContent="center"
-                        backgroundColor={objectFit === 'cover' ? 'black' : 'white'}
-                        alignItems="center"
-                        style={{ cursor: 'pointer' }}
-                    >
-                        {!imageLoaded && <Skeleton height="240px" width="100%" />}
-                        <ChakraImage
-                            src={imageSrc}
-                            alt={productName}
-                            objectFit={objectFit}
-                            height="100%"
-                            width="100%"
-                            onLoad={() => setImageLoaded(true)}
-                            display={imageLoaded ? 'block' : 'none'}
-                        />
-                    </Box>
+                    {!imageLoaded && <Skeleton height="240px" width="100%" />}
+                    <ChakraImage
+                        src={imageSrc}
+                        alt={productName}
+                        objectFit={objectFit}
+                        height="100%"
+                        width="100%"
+                        onLoad={() => setImageLoaded(true)}
+                        display={imageLoaded ? 'block' : 'none'}
+                    />
+                </Box>
+
+                <Flex
+                    p={{ base: '2', md: '4' }}
+                    // mb={{ base: '4', md: '0' }}
+                    flexDirection={'column'}
+                    gap={2}
+                    height={{ base: '129px', md: '161px' }} //161px
+                >
+                    <Flex alignItems="center" flexShrink={0}>
+                        <Text
+                            color="white"
+                            fontWeight="700"
+                            fontSize={{ base: '14px', md: '1.25rem' }}
+                            isTruncated
+                            noOfLines={2}
+                            overflow="hidden"
+                            textOverflow="ellipsis"
+                            whiteSpace="normal"
+                            wordBreak="break-word"
+                        >
+                            {productName}
+                        </Text>
+                    </Flex>
 
                     <Flex
-                        p={{ base: '2', md: '4' }}
-                        // mb={{ base: '4', md: '0' }}
-                        flexDirection={'column'}
                         gap={2}
-                        height={{ base: '129px', md: '161px' }} //161px
+                        mt="auto"
+                        mb={{ base: '0', md: '5px' }}
+                        flexDirection={'column'}
                     >
-                        <Flex alignItems="center" flexShrink={0}>
-                            <Text
-                                color="white"
-                                fontWeight="700"
-                                fontSize={{ base: '14px', md: '1.25rem' }}
-                                isTruncated
-                                noOfLines={2}
-                                overflow="hidden"
-                                textOverflow="ellipsis"
-                                whiteSpace="normal"
-                                wordBreak="break-word"
-                            >
-                                {productName}
-                            </Text>
-                        </Flex>
-
                         <Flex
-                            gap={2}
-                            mt="auto"
-                            mb={{ base: '0', md: '5px' }}
-                            flexDirection={'column'}
+                            alignItems="center"
+                            mb={{ base: '2.5px', md: '0' }}
                         >
-                            <Flex alignItems="center" mb={{ base: '2.5px', md: '0' }}>
-                                {reviewCount > 0 ? (
-                                    <>
-                                        <IoStar style={{ color: '#FEC84B' }} />
-                                        <Text
-                                            color={'white'}
-                                            alignSelf={'center'}
-                                            fontWeight="700"
-                                            fontSize={{ base: '14px', md: '14px' }}
-                                            ml={{ base: '1.5', md: '2' }}
-                                        >
-                                            {totalRating}
-                                        </Text>
-                                        <Text
-                                            alignSelf={'center'}
-                                            fontWeight="700"
-                                            fontSize={{ base: '14px', md: '16px' }}
-                                            color="#555555"
-                                            ml="2"
-                                        >
-                                            ({reviewCount}{' '}
-                                            {reviewCount === 1 ? 'review' : 'reviews'})
-                                        </Text>
-                                    </>
-                                ) : (
+                            {reviewCount > 0 ? (
+                                <>
+                                    <IoStar style={{ color: '#FEC84B' }} />
+                                    <Text
+                                        color={'white'}
+                                        alignSelf={'center'}
+                                        fontWeight="700"
+                                        fontSize={{ base: '14px', md: '14px' }}
+                                        ml={{ base: '1.5', md: '2' }}
+                                    >
+                                        {totalRating}
+                                    </Text>
                                     <Text
                                         alignSelf={'center'}
                                         fontWeight="700"
                                         fontSize={{ base: '14px', md: '16px' }}
-                                        color="white"
+                                        color="#555555"
                                         ml="2"
-                                    ></Text>
-                                )}
+                                    >
+                                        ({reviewCount}{' '}
+                                        {reviewCount === 1
+                                            ? 'review'
+                                            : 'reviews'}
+                                        )
+                                    </Text>
+                                </>
+                            ) : (
+                                <Text
+                                    alignSelf={'center'}
+                                    fontWeight="700"
+                                    fontSize={{ base: '14px', md: '16px' }}
+                                    color="white"
+                                    ml="2"
+                                ></Text>
+                            )}
+                        </Flex>
+
+                        <Flex
+                            flexDirection={{ base: 'column', md: 'row' }}
+                            mt="auto" // This pushes the content to the bottom
+                            alignItems={{ base: 'flex-start', md: 'center' }}
+                            justifyContent={{ base: 'flex-start', xl: '0' }}
+                        >
+                            <Flex mb="1px">
+                                <Image
+                                    className="h-[18px] w-[18px] md:h-[20px] md:w-[20px]"
+                                    src={currencyIcons[currencyCode ?? 'usdc']}
+                                    alt={currencyCode ?? 'usdc'}
+                                />
+                                <Text
+                                    display={'flex'}
+                                    flexDirection={'row'}
+                                    noOfLines={1}
+                                    color="white"
+                                    ml="2"
+                                    fontWeight="700"
+                                    fontSize={{ base: '14px', md: '18px' }}
+                                >
+                                    {productPrice}
+                                    <Text
+                                        textOverflow="ellipsis"
+                                        ml="5px"
+                                        as="span"
+                                        display={{
+                                            md: 'inline-block',
+                                        }}
+                                        style={{
+                                            fontSize: '12px',
+                                            color: 'white',
+                                            textTransform: 'uppercase',
+                                        }}
+                                    >
+                                        {currencyCode?.toUpperCase() || 'USDC'}
+                                    </Text>
+                                </Text>
                             </Flex>
 
-                            <Flex
-                                flexDirection={{ base: 'column', md: 'row' }}
-                                mt="auto" // This pushes the content to the bottom
-                                alignItems={{ base: 'flex-start', md: 'center' }}
-                                justifyContent={{ base: 'flex-start', xl: '0' }}
-                            >
-                                <Flex mb="1px">
-                                    <Image
-                                        className="h-[18px] w-[18px] md:h-[20px] md:w-[20px]"
-                                        src={currencyIcons[currencyCode ?? 'usdc']}
-                                        alt={currencyCode ?? 'usdc'}
-                                    />
+                            {currencyCode === 'eth' && (
+                                <Flex
+                                    flexDirection="row"
+                                    ml="5px"
+                                    display={{ base: 'none', md: 'flex' }}
+                                >
                                     <Text
+                                        textOverflow="ellipsis"
                                         display={'flex'}
                                         flexDirection={'row'}
                                         noOfLines={1}
                                         color="white"
-                                        ml="2"
-                                        fontWeight="700"
-                                        fontSize={{ base: '14px', md: '18px' }}
+                                        ml="auto"
+                                        fontWeight="500"
+                                        fontSize={{ base: '14px', md: '16px' }}
                                     >
-                                        {productPrice}
+                                        ≅ ${usdcProductPrice}
                                         <Text
                                             textOverflow="ellipsis"
                                             ml="5px"
@@ -211,51 +263,15 @@ const ProductCard: React.FC<ProductCardProps & { productId?: string }> = ({
                                                 textTransform: 'uppercase',
                                             }}
                                         >
-                                            {currencyCode?.toUpperCase() || 'USDC'}
+                                            USD
                                         </Text>
                                     </Text>
                                 </Flex>
-
-                                {currencyCode === 'eth' && (
-                                    <Flex
-                                        flexDirection="row"
-                                        ml="5px"
-                                        display={{ base: 'none', md: 'flex' }}
-                                    >
-                                        <Text
-                                            textOverflow="ellipsis"
-                                            display={'flex'}
-                                            flexDirection={'row'}
-                                            noOfLines={1}
-                                            color="white"
-                                            ml="auto"
-                                            fontWeight="500"
-                                            fontSize={{ base: '14px', md: '16px' }}
-                                        >
-                                            ≅ ${usdcProductPrice}
-                                            <Text
-                                                textOverflow="ellipsis"
-                                                ml="5px"
-                                                as="span"
-                                                display={{
-                                                    md: 'inline-block',
-                                                }}
-                                                style={{
-                                                    fontSize: '12px',
-                                                    color: 'white',
-                                                    textTransform: 'uppercase',
-                                                }}
-                                            >
-                                                USD
-                                            </Text>
-                                        </Text>
-                                    </Flex>
-                                )}
-                            </Flex>
+                            )}
                         </Flex>
                     </Flex>
-                </Box>
-            </ChakraLink>
+                </Flex>
+            </Box>
         </NextLink>
     );
 };
