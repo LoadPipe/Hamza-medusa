5ae8960e39dd39a2e7b9c870ac273045e968522e
Chris Li <cli@lotus-creations.com>
2025-04-11 15:22:19 +0800

    removed hydration to remove complexity for now

--- hamza-client/src/app/[countryCode]/(checkout)/checkout/page.tsx
+++ hamza-client/src/app/[countryCode]/(checkout)/checkout/page.tsx
@@ -1,11 +1,9 @@
 import { Metadata } from 'next';
 import { cookies } from 'next/headers';
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import { notFound } from 'next/navigation';
 import { Flex } from '@chakra-ui/react';
 import CheckoutTemplate from '@/modules/checkout/templates';
 import { fetchCartForCheckout } from '@/app/[countryCode]/(checkout)/checkout/utils/fetch-cart-for-checkout';
-import getQueryClient from '@/app/query-utils/getQueryClient';
 import { RainbowWrapper } from '@/app/components/providers/rainbowkit/rainbow-provider';
 import EmptyCart from '@/modules/cart/components/empty-cart';
 
@@ -15,9 +13,6 @@ export const metadata: Metadata = {
 };
 
 export default async function Checkout(params: any) {
-    // SSR So make sure to create a new queryClient instance, so we don't share the same instance between multiple requests
-    const queryClient = getQueryClient();
-
     let cartId = cookies().get('_medusa_cart_id')?.value;
     if (!cartId && params?.searchParams?.cart)
         cartId = params.searchParams.cart;
@@ -34,12 +29,10 @@ export default async function Checkout(params: any) {
     }
 
     return (
-        <HydrationBoundary state={dehydrate(queryClient)}>
-            <RainbowWrapper>
-                <Flex flexDir="row" maxW="1280px" width="100vw">
-                    {<CheckoutTemplate cart={cart} />}
-                </Flex>
-            </RainbowWrapper>
-        </HydrationBoundary>
+        <RainbowWrapper>
+            <Flex flexDir="row" maxW="1280px" width="100vw">
+                {<CheckoutTemplate cart={cart} />}
+            </Flex>
+        </RainbowWrapper>
     );
 }
