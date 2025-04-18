9d311bdd7cbe307a1b37d3e523845b472bdc9862
John Kosinski <john.kosinski@gmail.com>
2025-04-18 15:38:30 +0700

    Update preview-checkout.tsx

--- hamza-client/src/modules/products/components/product-preview/components/summary/preview-checkout.tsx
+++ hamza-client/src/modules/products/components/product-preview/components/summary/preview-checkout.tsx
@@ -473,13 +473,13 @@ const PreviewCheckout: React.FC<PreviewCheckoutProps> = ({
                         <Text fontSize={'18px'} fontWeight="normal" color="primary.green.900">
                             Escrow
                         </Text>
-                        <Box as="span" cursor="pointer" color="white">
-                            <Box as="svg" width="16px" height="16px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <circle cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="2" />
-                                <path d="M9 9a3 3 0 1 1 4 2.829 1 1 0 0 0-1 1V14" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
-                                <circle cx="12" cy="17" r="1" fill="currentColor" />
-                            </Box>
-                        </Box>
+                        { /* <Box as="span" cursor="pointer" color="white"> */ }
+                         { /*    <Box as="svg" width="16px" height="16px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"> */ }
+                        { /*         <circle cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="2" /> */ }
+                         { /*        <path d="M9 9a3 3 0 1 1 4 2.829 1 1 0 0 0-1 1V14" stroke="currentColor" strokeWidth="2" strokeLinecap="round" /> */ }
+                         { /*        <circle cx="12" cy="17" r="1" fill="currentColor" /> */ }
+                       { /*      </Box> */ }
+                       { /*  </Box> */}
                     </Flex>
                 </Flex>
                 <Flex
