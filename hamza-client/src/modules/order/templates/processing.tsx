7bf3c8628e122e86231a92d12eb51e0fe10e11c7
jrkosinski <john.kosinski@gmail.com>
2025-05-02 13:26:46 +0700

    colorized link

--- hamza-client/src/modules/order/templates/processing.tsx
+++ hamza-client/src/modules/order/templates/processing.tsx
@@ -530,32 +530,6 @@ const Processing = ({
                                                                                     item.currency_code
                                                                                 )}
                                                                             </Text>
-
-                                                                            <Flex
-                                                                                align="center"
-                                                                                gap={
-                                                                                    2
-                                                                                }
-                                                                            >
-                                                                                <a
-                                                                                    href={
-                                                                                        process
-                                                                                            .env
-                                                                                            .NEXT_PUBLIC_HAMZA_CHAT_LINK
-                                                                                            ? `${process.env.NEXT_PUBLIC_HAMZA_CHAT_LINK}?target=${order.store.handle}.hamzamarket&order=${order.id}`
-                                                                                            : 'https://support.hamza.market/help/1568263160'
-                                                                                    }
-                                                                                    target="_blank"
-                                                                                >
-                                                                                    <Text fontSize="md">
-                                                                                        <strong>
-                                                                                            Chat
-                                                                                            with
-                                                                                            Merchant
-                                                                                        </strong>{' '}
-                                                                                    </Text>
-                                                                                </a>
-                                                                            </Flex>
                                                                         </VStack>
 
                                                                         {/* Right Column: Order ID & Chain Data */}
@@ -617,6 +591,37 @@ const Processing = ({
                                                                                         chainId
                                                                                     )}
                                                                                 </Text>
+                                                                            </Flex>
+
+                                                                            <Flex
+                                                                                align="center"
+                                                                                gap={
+                                                                                    2
+                                                                                }
+                                                                            >
+                                                                                <a
+                                                                                    href={
+                                                                                        process
+                                                                                            .env
+                                                                                            .NEXT_PUBLIC_HAMZA_CHAT_LINK
+                                                                                            ? `${process.env.NEXT_PUBLIC_HAMZA_CHAT_LINK}?target=${order.store.handle}.hamzamarket&order=${order.id}`
+                                                                                            : 'https://support.hamza.market/help/1568263160'
+                                                                                    }
+                                                                                    target="_blank"
+                                                                                >
+                                                                                    <Text
+                                                                                        fontSize="md"
+                                                                                        color={
+                                                                                            '#ADD8E6'
+                                                                                        }
+                                                                                    >
+                                                                                        <strong>
+                                                                                            Chat
+                                                                                            with
+                                                                                            Merchant
+                                                                                        </strong>{' '}
+                                                                                    </Text>
+                                                                                </a>
                                                                             </Flex>
                                                                         </VStack>
                                                                     </Flex>
