818e52cabbaf0dc62e8391a6d2cd9fbab9715886
Chris Li <cli@lotus-creations.com>
2025-07-28 10:25:32 +0800

    Removed the nested MenuList structure - there were two MenuList components nested inside each other Flattened to a single MenuList - now matches the working mobile main menu pattern Consolidated all styling properties into the single MenuList component

--- hamza-client/src/modules/nav/templates/nav/menu-mobile/menu/mobile-account-menu.tsx
+++ hamza-client/src/modules/nav/templates/nav/menu-mobile/menu/mobile-account-menu.tsx
@@ -42,6 +42,7 @@ const MobileAccountMenu = () => {
                 backgroundColor={'transparent'}
                 cursor={'pointer'}
                 position="relative"
+                className="mobile-account-menu-button"
                 _hover={{
                     '.profile-icon': {
                         color: 'primary.green.900',
@@ -73,30 +74,45 @@ const MobileAccountMenu = () => {
             </MenuButton>
             <MenuList
                 marginTop={'1rem'}
-                px={'1rem'}
-                py={'0px'}
-                borderWidth={'0px'}
-                backgroundColor={'transparent'}
-                justifyContent={'center'}
+                borderRadius={'16px'}
+                borderColor={'#555555'}
+                borderWidth={'1px'}
+                pb={'0px'}
+                backgroundColor={'#121212'}
                 width={`calc(100vw)`}
             >
-                <MenuList
-                    borderRadius={'16px'}
-                    borderColor={'#555555'}
-                    borderWidth={'1px'}
-                    pb={'0px'}
-                    width={'100%'}
-                    backgroundColor={'#121212'}
-                >
-                    <Wishlist />
+                <Wishlist />
 
-                    <MenuDivider borderColor={'#555555'} />
+                <MenuDivider borderColor={'#555555'} />
 
-                    <Link href={`/account/profile`}>
+                <Link href={`/account/profile`}>
+                    <MenuItem
+                        fontWeight={'600'}
+                        my="1rem"
+                        pl="1rem"
+                        color={'white'}
+                        backgroundColor={'#121212'}
+                        _hover={{ color: 'primary.green.900' }}
+                    >
+                        <Flex
+                            w={'30px'}
+                            alignContent={'center'}
+                            justifyContent={'center'}
+                        >
+                            <MdOutlinePersonOutline size={30} />
+                        </Flex>
+                        <Text fontWeight={'600'} ml="0.5rem">
+                            Manage My Account
+                        </Text>
+                    </MenuItem>
+                </Link>
+                {authData.is_verified === false && (
+                    <Link href={`/account/verify`}>
                         <MenuItem
                             fontWeight={'600'}
-                            my="1rem"
                             pl="1rem"
+                            mt="-1rem"
+                            mb="1rem"
                             color={'white'}
                             backgroundColor={'#121212'}
                             _hover={{ color: 'primary.green.900' }}
@@ -106,40 +122,16 @@ const MobileAccountMenu = () => {
                                 alignContent={'center'}
                                 justifyContent={'center'}
                             >
-                                <MdOutlinePersonOutline size={30} />
+                                <MdOutlineShield size={29} />
                             </Flex>
                             <Text fontWeight={'600'} ml="0.5rem">
-                                Manage My Account
+                                Verify Account
                             </Text>
                         </MenuItem>
                     </Link>
-                    {authData.is_verified === false && (
-                        <Link href={`/account/verify`}>
-                            <MenuItem
-                                fontWeight={'600'}
-                                pl="1rem"
-                                mt="-1rem"
-                                mb="1rem"
-                                color={'white'}
-                                backgroundColor={'#121212'}
-                                _hover={{ color: 'primary.green.900' }}
-                            >
-                                <Flex
-                                    w={'30px'}
-                                    alignContent={'center'}
-                                    justifyContent={'center'}
-                                >
-                                    <MdOutlineShield size={29} />
-                                </Flex>
-                                <Text fontWeight={'600'} ml="0.5rem">
-                                    Verify Account
-                                </Text>
-                            </MenuItem>
-                        </Link>
-                    )}
-                    <MenuDivider mb="0" borderColor={'#555555'} />
-                    <AuthorizedAccount />
-                </MenuList>
+                )}
+                <MenuDivider mb="0" borderColor={'#555555'} />
+                <AuthorizedAccount />
             </MenuList>
         </Menu>
     );
