eacdb909496a4d6489855cb21ec671a980daea58
Chris Li <cli@lotus-creations.com>
2025-07-29 14:53:07 +0800

    rainbowkit wallet automation isn't working, disable and just do manual connect.

--- hamza-client/cypress/e2e/headed/features/login.cy.js
+++ hamza-client/cypress/e2e/headed/features/login.cy.js
@@ -1,6 +1,7 @@
 import { buttonClickByElementClass } from '../../../support/utils/buttons/button-click-by-element-class';
 import { elementCheckByElementText } from '../../../support/utils/element-check-by-element-text';
 import { buttonClickByElementText } from '../../../support/utils/buttons/button-click-by-element-text';
+import { connectWallet } from '../../../support/utils/buttons/connect-wallet';
 
 describe('Metamask login then user profile', () => {
     beforeEach(() => {
@@ -8,23 +9,7 @@ describe('Metamask login then user profile', () => {
     });
 
     it('should connect wallet and display account information', () => {
-        buttonClickByElementText('Connect Wallet');
-
-        // Simulate wallet selection (MetaMask in this case)
-        cy.get('body').then(($body) => {
-            if ($body.find('button:contains("MetaMask")').length) {
-                buttonClickByElementText('MetaMask');
-            }
-        });
-
-        buttonClickByElementText('Sign message');
-
-        elementCheckByElementText('Verify your account', {
-            exist: false,
-            scrollIntoView: false,
-            beVisible: false,
-            timeout: 30000,
-        });
+        connectWallet();
 
         //open user menu
         buttonClickByElementClass('.account-menu-button');
