bc160da4e78828338626b7d9a5b7fbfdce72e138
Chris Li <cli@lotus-creations.com>
2025-07-29 13:21:16 +0800

    Update search feature tests to enhance placeholder text and ensure search results validation

--- hamza-client/cypress/e2e/headless/features/search.cy.js
+++ hamza-client/cypress/e2e/headless/features/search.cy.js
@@ -1,17 +1,22 @@
 // test for header
 describe('Search', () => {
-	beforeEach(() => {
-			cy.visit('/en');
-	});
+    beforeEach(() => {
+        cy.visit('/en');
+    });
 
-	it('should have search bar', () => {
-		cy.get('input[placeholder="Search for product name, product..."]').should('exist').click();
+    it('should have search bar', () => {
+        cy.get(
+            'input[placeholder="Search for product name, product type, brand name, category, etc..."]'
+        )
+            .should('exist')
+            .click();
 
-		cy.get('input[placeholder="Search products..."]').should('exist').type('tshirt');
-		
-		cy.get('.search-results').should('exist');
+        cy.get('input[placeholder="Search products..."]')
+            .should('exist')
+            .type('tshirt');
 
-		cy.get('.search-results').find('p').contains(/shirt/i);
-		
-	});
+        cy.get('.search-results').should('exist');
+
+        cy.get('.search-results').find('p').contains(/shirt/i);
+    });
 });
